d31e499e4bc77505532fa65dbd5ad9654e134060
joeresu <joules.castillon@gmail.com>
2020-05-29 12:57:17 +0800

    update workspace header

--- src/components/panels/workspace/WorkspaceHeaderPanel.jsx
+++ src/components/panels/workspace/WorkspaceHeaderPanel.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useRouteMatch} from "react-router-dom";
 import styled from "styled-components";
+import {addToModals} from "../../../redux/actions/globalActions";
 import {Avatar, SvgIconFeather} from "../../common";
 
 const NavBar = styled.ul`
@@ -13,19 +14,45 @@ const NavBar = styled.ul`
 
 const WorkspaceName = styled.h2`
     margin-right: 1rem;
-    letter-spacing: -0.5px;
+    letter-spacing: 0;
     margin-bottom: 0;
-    color: #82828A;
-    font-weight: 500;
+    color: #64625C;
+    font-weight: 600;
+    font-size: 19px;
 `;
 
 const SubWorkspaceName = styled.h3`
-    font-size: 20px;    
-    letter-spacing: -0.5px;
+    margin-right: 1rem;
+    letter-spacing: 0;
     margin-bottom: 0;
-    color: #82828A;
-    font-weight: 500;
+    color: #B8B8B8;
+    font-weight: normal;
+    font-size: 19px;
     text-transform: lowercase;
+    
+    &:before {
+        content: '';
+        display: inline-block;
+        width: 4px;
+        height: 4px;
+        -moz-border-radius: 7.5px;
+        -webkit-border-radius: 7.5px;
+        border-radius: 7.5px;
+        background-color: #B8B8B8;
+        margin-right: 12px;
+        position: relative;
+        top: -4px;
+    }
+`;
+
+const WorkspaceButton = styled.h3`
+    cursor: pointer;
+    cursor: hand;
+    
+    svg {
+        fill: #505050;
+        transform: rotate(90deg);
+    }
 `;
 
 const WorspaceHeaderPanel = (props) => {
@@ -33,6 +60,18 @@ const WorspaceHeaderPanel = (props) => {
     const dispatch = useDispatch();
     const match = useRouteMatch();
     const user = useSelector(state => state.session.user);
+    const activeTopic = useSelector(state => state.workspaces.activeTopic);
+
+    const handleShowWorkspaceModal = () => {
+        let payload = {
+            type: "workspace_create_edit",
+            mode: "create",
+        };
+
+        dispatch(
+            addToModals(payload),
+        );
+    };
 
     useEffect(() => {
         const body = document.body;
@@ -49,12 +88,24 @@ const WorspaceHeaderPanel = (props) => {
                             <SvgIconFeather icon="menu"/>
                         </a>
                     </li>
-                    <li className="nav-item">
-                        <WorkspaceName>Workspace</WorkspaceName>
-                    </li>
-                    <li className="nav-item">
-                        <SubWorkspaceName>Sub workspace item</SubWorkspaceName>
-                    </li>
+                    {
+                        activeTopic ?
+                            <>
+                                <li className="nav-item">
+                                    <WorkspaceName>{activeTopic.workspace_name}</WorkspaceName>
+                                </li>
+                                <li className="nav-item">
+
+                                    <SubWorkspaceName>{activeTopic.name}</SubWorkspaceName>
+                                </li>
+                            </>
+                            :
+                            <>
+                                <li className="nav-item">
+                                    <WorkspaceButton onClick={handleShowWorkspaceModal}>New workspace <SvgIconFeather icon="play" /></WorkspaceButton>
+                                </li>
+                            </>
+                    }
                 </NavBar>
             </div>
 
