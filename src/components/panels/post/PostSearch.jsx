d32fb11b4c3cfa0c7ddc2f4d8e66d5446456c02d
Nilo Cruz <nilo@makedevelopment.com>
2020-06-06 00:00:29 +0800

    Driff-157
    updated usegetworkspaceposts hook by adding filter on search results
    store the search results on workspace posts

--- src/components/panels/post/PostSearch.jsx
+++ src/components/panels/post/PostSearch.jsx
@@ -1,32 +1,51 @@
 import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {useParams} from "react-router-dom";
-import {getWorkspacePosts} from "../../../redux/actions/workspaceActions";
+import {getWorkspacePosts, addPostSearchResult} from "../../../redux/actions/workspaceActions";
 
 const PostSearch = props => {
 
     const dispatch = useDispatch();
     const params = useParams();
     const [searchValue, setSearchValue] = useState("");
+    let topic_id = parseInt(params.workspaceId);
 
     const handleInputChange = e => {
+        if (searchValue.trim() === "") handleClearSearchPosts();
         setSearchValue(e.target.value);
+    };
+
+    const handleClearSearchPosts = () => {
+        dispatch(
+            addPostSearchResult({
+                topic_id: topic_id,
+                search_result: []
+            })
+        );
     };
 
     const handleEnter = e => {
         if (e.key === "Enter") {
             handleSearch();
         }
-    }
+    };
+
     const handleSearch = () => {
         dispatch(
             getWorkspacePosts({
-                topic_id: parseInt(params.workspaceId),
+                topic_id: topic_id,
                 search: searchValue
             }, (err,res) => {
+                if (err) return;
+                dispatch(
+                    addPostSearchResult({
+                        topic_id: topic_id,
+                        search_result: res.data.posts
+                    })
+                )
                 console.log(res, 'search result')
             })
-        )
+        );
     };
 
     return (
--- src/redux/reducers/workspacesReducer.js
+++ src/redux/reducers/workspacesReducer.js
@@ -384,6 +384,18 @@ export default (state = INITIAL_STATE, action) => {
                 activeTab: action.data
             }
         }
+        case "ADD_POST_SEARCH_RESULT": {
+            return {
+                ...state,
+                workspacePosts: {
+                    ...state.workspacePosts,
+                    [action.data.topic_id]: {
+                        ...state.workspacePosts[action.data.topic_id],
+                        searchResults: action.data.search_result
+                    }
+                }
+            }
+        }
         case "ADD_TO_WORKSPACE_POSTS": {
             let convertedPosts = convertArrayToObject(action.data.posts, "id");
             return {
@@ -394,6 +406,7 @@ export default (state = INITIAL_STATE, action) => {
                         filter: "all",
                         sort: null,
                         tag: null,
+                        searchResults: [],
                         posts: {
                             ...[action.data.topic_id].posts,
                             ...convertedPosts
