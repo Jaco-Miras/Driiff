44c00fae42d7d3f002e8a385f6f3fa98a0019ecc
joeresu <joules.castillon@gmail.com>
2020-06-19 12:49:45 +0800

    display file size and unit

--- src/components/list/file/item/FileListItem.jsx
+++ src/components/list/file/item/FileListItem.jsx
@@ -19,6 +19,20 @@ const FileListItem = (props) => {
 
     const {className = "", file, scrollRef, actions} = props;
 
+    let fileSize = 0;
+    let fileSizeUnit = "";
+
+    if (file.size < 1e+6) {
+        fileSize = file.size / 1000;
+        fileSizeUnit = "KB";
+    } else if (file.size < 1e+9) {
+        fileSize = file.size / 1e+6;
+        fileSizeUnit = "MB";
+    } else if (file.size < 1e+12) {
+        fileSize = file.size / 1e+9`$ GB`;
+        fileSizeUnit = "GB";
+    }
+
     return (
         <Wrapper className={`file-list-item ${className}`}>
             <div className="card app-file-list">
@@ -28,7 +42,8 @@ const FileListItem = (props) => {
                 </div>
                 <div className="p-2 small">
                     <div>{file.search}</div>
-                    <div className="text-muted">{file.size}</div>
+                    <div
+                        className="text-muted">{(fileSize).toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]}{fileSizeUnit}</div>
                 </div>
             </div>
         </Wrapper>
