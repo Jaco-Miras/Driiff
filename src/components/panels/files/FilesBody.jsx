6c22eeea144a2a3a13acff51f059ae2fbb73efb4
joeresu <joules.castillon@gmail.com>
2020-06-20 09:42:53 +0800

    DRIFF-264 hide option when empty

--- src/components/panels/files/FilesBody.jsx
+++ src/components/panels/files/FilesBody.jsx
@@ -119,6 +119,7 @@ const FilesBody = (props) => {
     const handleEditFolder = () => {
         handleAddEditFolder("update");
     };
+
     return (
         <Wrapper className={`files-body card app-content-body ${className}`} onDragOver={handleShowDropZone}>
             <DropDocument
@@ -133,115 +134,119 @@ const FilesBody = (props) => {
             />
             <div className="card-body">
                 {
-                    typeof wsFiles !== "undefined" && fileIds.length === 0 && filter === "" && search === "" ?
-                    <EmptyState>
-                        <SvgEmptyState icon={4} height={282}/>
-                        {
-                            isMember &&
-                            <button className="btn btn-outline-primary btn-block"
-                                    onClick={handleShowUploadModal}>
-                                Upload files
-                            </button>
-                        }
-                    </EmptyState>
-                                                                                                             :
                     typeof wsFiles !== "undefined" &&
-                        <>
-                            {
-                                folder &&
-                                <MoreButton moreButton="settings">
-                                    <div onClick={handleEditFolder}>Edit folder</div>
-                                    <div onClick={handleRemoveFolder}>Remove folder</div>
-                                </MoreButton>
-                            }
-                            {
-                                filter === "removed" &&
-                                wsFiles && wsFiles.hasOwnProperty("trash_files") && Object.keys(wsFiles.trash_files).length > 0 &&
-                                <SvgIconFeather icon="trash" onClick={actions.removeTrashFiles}/>
-                            }
-                            {
-                                filter === "" &&
-                                <>
-                                    <h6 className="font-size-11 text-uppercase mb-4">{folder ? folder.search : "All files"}</h6>
-                                    <div className="row">
-                                        {
-                                            wsFiles &&
-                                            fileIds.map(f => {
-                                                return (
-                                                    <FileListItem scrollRef={scrollRef} key={f.id} actions={actions}
-                                                                  className="col-xl-3 col-lg-4 col-md-6 col-sm-12"
-                                                                  file={wsFiles.files[f]}/>
-                                                );
-                                            })
-                                        }
-                                    </div>
+                    <>
+                        {
+                            folder && isMember && fileIds.length !== 0 &&
+                            <MoreButton moreButton="settings">
+                                <div onClick={handleEditFolder}>Edit folder</div>
+                                <div onClick={handleRemoveFolder}>Remove folder</div>
+                            </MoreButton>
+                        }
+                        {
+                            filter === "removed" &&
+                            wsFiles && wsFiles.hasOwnProperty("trash_files") && Object.keys(wsFiles.trash_files).length > 0 &&
+                            <SvgIconFeather icon="trash" onClick={actions.removeTrashFiles}/>
+                        }
+                        {
+                            filter === "" &&
+                            <>
+                                <h6 className="font-size-11 text-uppercase mb-4">{folder ? folder.search : "All files"}</h6>
+                                <div className="row">
                                     {
-                                        wsFiles && wsFiles.popular_files.length > 0 && folder === null &&
-                                        <PopularFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
+                                        wsFiles &&
+                                        fileIds.map(f => {
+                                            return (
+                                                <FileListItem scrollRef={scrollRef} key={f.id} actions={actions}
+                                                              className="col-xl-3 col-lg-4 col-md-6 col-sm-12"
+                                                              file={wsFiles.files[f]}/>
+                                            );
+                                        })
+                                    }
+                                </div>
+                                {
+                                    wsFiles && wsFiles.popular_files.length > 0 && folder === null &&
+                                    <PopularFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
+                                                  actions={actions}/>
+                                }
+                                {
+                                    wsFiles && wsFiles.recently_edited.length > 0 && folder === null &&
+                                    <RecentEditedFile search={search} scrollRef={scrollRef} wsFiles={wsFiles}
                                                       actions={actions}/>
-                                    }
-                                    {
-                                        wsFiles && wsFiles.recently_edited.length > 0 && folder === null &&
-                                        <RecentEditedFile search={search} scrollRef={scrollRef} wsFiles={wsFiles}
-                                                          actions={actions}/>
-                                    }
-                                </>
-                            }
-                            {
-                                filter === "recent" &&
-                                <>
-                                    <RecentEditedFile
-                                        search={search} scrollRef={scrollRef} wsFiles={wsFiles}
-                                        actions={actions}/>
-                                    {
-                                        !(wsFiles && wsFiles.recently_edited.length > 0) &&
-                                        <EmptyState>
-                                            <SvgEmptyState icon={4} height={282}/>
-                                            {
-                                                isMember &&
-                                                <button className="btn btn-outline-primary btn-block"
-                                                        onClick={handleShowUploadModal}>
-                                                    Upload files
-                                                </button>
-                                            }
-                                        </EmptyState>
-                                    }
-                                </>
-                            }
-                            {
-                                filter === "important" &&
-                                <>
-                                    <ImportantFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
-                                                    actions={actions}/>
-                                    {
-                                        !(wsFiles && wsFiles.hasOwnProperty("favorite_files") && wsFiles.favorite_files.length > 0) &&
-                                        <EmptyState>
-                                            <SvgEmptyState icon={4} height={282}/>
-                                            {
-                                                isMember &&
-                                                <button className="btn btn-outline-primary btn-block"
-                                                        onClick={handleShowUploadModal}>
-                                                    Upload files
-                                                </button>
-                                            }
-                                        </EmptyState>
-                                    }
-                                </>
-                            }
-                            {
-                                filter === "removed" &&
-                                <>
-                                    <RemoveFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
-                                                 actions={actions}/>
-                                    {
-                                        !(wsFiles && wsFiles.hasOwnProperty("trash_files") && Object.keys(wsFiles.trash_files).length > 0) &&
-                                        <EmptyState>
-                                            <SvgEmptyState icon={4} height={282}/>
-                                        </EmptyState>
-                                    }
-                                </>
-                            }
-                        </>
+                                }
+
+                                {
+                                    wsFiles &&
+                                    (wsFiles.popular_files.length === 0 && wsFiles.recently_edited.length === 0) &&
+                                    fileIds.length === 0 &&
+                                    <EmptyState>
+                                        <SvgEmptyState icon={4} height={282}/>
+                                        {
+                                            isMember &&
+                                            <button className="btn btn-outline-primary btn-block"
+                                                    onClick={handleShowUploadModal}>
+                                                Upload files
+                                            </button>
+                                        }
+                                    </EmptyState>
+                                }
+                            </>
+                        }
+                        {
+                            filter === "recent" &&
+                            <>
+                                <RecentEditedFile
+                                    search={search} scrollRef={scrollRef} wsFiles={wsFiles}
+                                    actions={actions}/>
+                                {
+                                    !(wsFiles && wsFiles.recently_edited.length > 0) &&
+                                    <EmptyState>
+                                        <SvgEmptyState icon={4} height={282}/>
+                                        {
+                                            isMember &&
+                                            <button className="btn btn-outline-primary btn-block"
+                                                    onClick={handleShowUploadModal}>
+                                                Upload files
+                                            </button>
+                                        }
+                                    </EmptyState>
+                                }
+                            </>
+                        }
+                        {
+                            filter === "important" &&
+                            <>
+                                <ImportantFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
+                                                actions={actions}/>
+                                {
+                                    !(wsFiles && wsFiles.hasOwnProperty("favorite_files") && wsFiles.favorite_files.length > 0) &&
+                                    <EmptyState>
+                                        <SvgEmptyState icon={4} height={282}/>
+                                        {
+                                            isMember &&
+                                            <button className="btn btn-outline-primary btn-block"
+                                                    onClick={handleShowUploadModal}>
+                                                Upload files
+                                            </button>
+                                        }
+                                    </EmptyState>
+                                }
+                            </>
+                        }
+                        {
+                            filter === "removed" &&
+                            <>
+                                <RemoveFiles search={search} scrollRef={scrollRef} wsFiles={wsFiles}
+                                             actions={actions}/>
+                                {
+                                    !(wsFiles && wsFiles.hasOwnProperty("trash_files") && Object.keys(wsFiles.trash_files).length > 0) &&
+                                    <EmptyState>
+                                        <SvgEmptyState icon={4} height={282}/>
+                                    </EmptyState>
+                                }
+                            </>
+                        }
+                    </>
                 }
             </div>
         </Wrapper>
