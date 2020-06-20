97205bee2fee37346720ba947434b424a6208437
joeresu <joules.castillon@gmail.com>
2020-06-20 09:56:52 +0800

    DRIFF-264 allow showing multiple post badge

--- src/components/panels/post/PostBadge.jsx
+++ src/components/panels/post/PostBadge.jsx
@@ -6,21 +6,32 @@ const PostBadge = props => {
 
     if (post.is_must_read !== 0 || post.is_must_reply !== 0 || post.is_read_only !== 0) {
         return (
-            <div className="mr-3 d-sm-inline d-none">
-                <div className={`badge 
-                    ${post.is_must_read === 1 ?
-                      "badge-danger" :
-                      post.is_must_reply === 1 ?
-                      "badge-warning" :
-                      post.is_read_only === 1 ?
-                      "badge-info" :
-                      "badge-primary"}
-                `}>
-                    {
-                        post.is_must_read === 1 ? "Must read" : post.is_must_reply === 1 ? "Reply required" : "No replies"
-                    }
-                </div>
-            </div>
+            <>
+                {
+                    post.is_must_read === 1 &&
+                    <div className="mr-3 d-sm-inline d-none">
+                        <div className="badge badge-danger">
+                            Must read
+                        </div>
+                    </div>
+                }
+                {
+                    post.is_must_reply === 1 &&
+                    <div className="mr-3 d-sm-inline d-none">
+                        <div className="badge badge-warning">
+                            Reply required
+                        </div>
+                    </div>
+                }
+                {
+                    post.is_read_only === 1 &&
+                    <div className="mr-3 d-sm-inline d-none">
+                        <div className="badge badge-info">
+                            No replies
+                        </div>
+                    </div>
+                }
+            </>
         );
     } else {
         return null;
