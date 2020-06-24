876efccae8752fc3ecefcefe8ed4967aa34687bc
joeresu <joules.castillon@gmail.com>
2020-06-24 22:34:32 +0800

    add save icon

--- src/components/common/SvgIcon.jsx
+++ src/components/common/SvgIcon.jsx
@@ -288,6 +288,13 @@ export const SvgIconFeather = React.memo(React.forwardRef((props, ref) => {
                 <line x1="1" y1="1" x2="23" y2="23"/>
             </>;
             break;
+        case "save":
+            content = <>
+                <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/>
+                <polyline points="17 21 17 13 7 13 7 21"/>
+                <polyline points="7 3 7 8 15 8"/>
+            </>;
+            break;
         case "smile":
             content = <>
                 <circle cx="12" cy="12" r="10"/>
