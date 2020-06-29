ded46d84a5db5a5ba0167632aa07311197e9a28e
joeresu <joules.castillon@gmail.com>
2020-06-29 10:47:18 +0800

    Driff-166 - pill  size - draft label

--- src/components/panels/post/PostBadge.jsx
+++ src/components/panels/post/PostBadge.jsx
@@ -4,9 +4,17 @@ const PostBadge = props => {
 
     const {post} = props;
 
-    if (post.is_must_read !== 0 || post.is_must_reply !== 0 || post.is_read_only !== 0) {
+    if (post.is_must_read !== 0 || post.is_must_reply !== 0 || post.is_read_only !== 0 || post.type === "draft_post") {
         return (
             <>
+                {
+                    post.type === "draft_post" &&
+                    <div className="mr-3 d-sm-inline d-none">
+                        <div className="badge badge-light text-white">
+                            Draft
+                        </div>
+                    </div>
+                }
                 {
                     post.is_must_read === 1 &&
                     <div className="mr-3 d-sm-inline d-none">
@@ -34,7 +42,7 @@ const PostBadge = props => {
             </>
         );
     } else {
-        return null;
+        return <></>;
     }
 
 };
