2d7744038cbb6ac9f66c9d0938ebcd435c5d00a0
joeresu <joules.castillon@gmail.com>
2020-06-30 11:16:33 +0800

    Driff-347 add check icon add notification timeline work on overview page design requirement

--- src/components/panels/user/UserNotificationPanel.jsx
+++ src/components/panels/user/UserNotificationPanel.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
 import styled from "styled-components";
-import {useNotifications, useNotificationActions} from "../../hooks";
-import {NotificationLists} from "../../list/notification/item";
+import {useNotificationActions, useNotifications} from "../../hooks";
+import {NotificationTimelineItem} from "../../list/notification/item";
 
 const Wrapper = styled.div`    
 `;
@@ -10,24 +11,41 @@ const UserNotificationPanel = (props) => {
 
     const {className = ""} = props;
 
+    const history = useHistory();
     const actions = useNotificationActions();
     const {notifications} = useNotifications();
 
     return (
         <Wrapper className={`user-profile-panel container-fluid h-100 ${className}`}>
-            <div className="row row-user-profile-panel">
+            <div className="row row-user-profile-panel justify-content-center">
                 <div className="col-md-6">
                     <div className="card">
-                        <h6>Notifications</h6>
-                        {
-                            Object.keys(notifications).length > 0 &&
-                            <div>
-                                <span className="mr-2" onClick={actions.readAll}>Mark all as read</span>
-                                {/* <span onClick={actions.removeAll}>Remove all</span> */}
+                        <div className="card-body">
+                            <h6 className="card-title d-flex justify-content-between align-items-center">
+                                <div>
+                                    Notifications
+                                </div>
+                                {
+                                    Object.keys(notifications).length > 0 &&
+                                    <div>
+                                        <span className="cursor-pointer" onClick={actions.readAll}>
+                                            <i className="hide-show-toggler-item fa fa-check font-size-16 mr-2"/>
+                                            Mark all as read
+                                        </span>
+                                    </div>
+                                }
+                            </h6>
+                            <div className="timeline">
+                                {
+                                    Object.values(notifications).map(n => {
+                                        return <NotificationTimelineItem
+                                            key={n.id} notification={n} actions={actions}
+                                            history={history}/>;
+                                    })
+
+                                }
                             </div>
-                        }
-                        
-                        <NotificationLists notifications={notifications} actions={actions}/>
+                        </div>
                     </div>
                 </div>
             </div>
