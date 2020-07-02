e3040c4a1902fa79227dec590ef6b90b2ca8f363
joeresu <joules.castillon@gmail.com>
2020-07-02 16:20:04 +0800

    add 2 second duration on toaster

--- src/components/hooks/useToaster.jsx
+++ src/components/hooks/useToaster.jsx
@@ -1,6 +1,7 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import styled from "styled-components";
 import toaster from "toasted-notes";
+import {ProgressBar} from "../panels/common";
 
 const Wrapper = styled.div`
     position: initial !important;    
@@ -9,30 +10,45 @@ const Wrapper = styled.div`
 const useToaster = () => {
 
     const success = useCallback((text, options = {}) => {
+        if (!options.hasOwnProperty("duration")) {
+            options.duration = 2000;
+        }
         toaster.notify(() => <Wrapper id="toast-container">
             <div className="toast-success">{text}</div>
         </Wrapper>, options);
     }, []);
 
     const error = useCallback((text, options = {}) => {
+        if (!options.hasOwnProperty("duration")) {
+            options.duration = 2000;
+        }
         toaster.notify(() => <Wrapper id="toast-container">
             <div className="toast-error">{text}</div>
         </Wrapper>, options);
     }, []);
 
     const info = useCallback((text, options = {}) => {
+        if (!options.hasOwnProperty("duration")) {
+            options.duration = 2000;
+        }
         toaster.notify(() => <Wrapper id="toast-container">
             <div className="toast-info">{text}</div>
         </Wrapper>, options);
     }, []);
 
     const warning = useCallback((text, options = {}) => {
+        if (!options.hasOwnProperty("duration")) {
+            options.duration = 2000;
+        }
         toaster.notify(() => <Wrapper id="toast-container">
             <div className="toast-warning">{text}</div>
         </Wrapper>, options);
     }, []);
 
     const notify = useCallback((text, options = {}) => {
+        if (!options.hasOwnProperty("duration")) {
+            options.duration = 2000;
+        }
         info(text, options);
     }, []);
 
