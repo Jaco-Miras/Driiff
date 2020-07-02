9c002ce8421751c1062c64c5097eb8750a7225dd
Nilo Cruz <nilo@makedevelopment.com>
2020-07-02 14:18:20 +0800

    Driff-379
    Fixed issue on dashboard team.
    update workspace reducer

--- src/components/panels/dashboard/DashboardTeam.jsx
+++ src/components/panels/dashboard/DashboardTeam.jsx
@@ -1,8 +1,6 @@
 import React, {useCallback, useState} from "react";
 import styled from "styled-components";
 import {TeamListItem} from "../../list/people/item";
-import {addToModals} from "../../../redux/actions/globalActions";
-import {useDispatch} from "react-redux";
 import {SvgIconFeather} from "../../common";
 
 const Wrapper = styled.div`
@@ -62,26 +60,13 @@ const DashboardTeam = (props) => {
         //eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const dispatch = useDispatch();
-
-    const handleShowWorkspaceModal = () => {
-        let payload = {
-            type: "workspace_create_edit",
-            mode: "edit",
-        };
-
-        dispatch(
-            addToModals(payload),
-        );
-    };
-
     if (!workspace)
         return <></>;
 
     return (
         <Wrapper className={`dashboard-team card ${className}`}>
             <div ref={assignRef} className="card-body">
-                <h5 className="card-title">Team <SvgIconFeather onClick={handleShowWorkspaceModal} icon="plus"/></h5>
+                <h5 className="card-title">Team <SvgIconFeather onClick={onEditClick} icon="plus"/></h5>
 
 
 
