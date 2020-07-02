58d813a15fb0d6c4a2700a7ec2611af589b65542
Nilo Cruz <nilo@makedevelopment.com>
2020-07-02 12:53:23 +0800

    Driff-266
    fixed merge issue

--- src/components/panels/files/RemoveFiles.jsx
+++ src/components/panels/files/RemoveFiles.jsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
 
 const RemoveFiles = (props) => {
 
-    const {className = "", actions, wsFiles, isMember, folder} = props;
+    const {className = "", actions, wsFiles, isMember, folder, params} = props;
 
     // const files = [{
     //     id: 1,
@@ -26,6 +26,7 @@ const RemoveFiles = (props) => {
             <h6 className="font-size-11 text-uppercase mb-4">Removed</h6>
             <div className="row">
                 {
+                    params.hasOwnProperty("fileFolderId") ?
                     wsFiles && Object.values(wsFiles.trash_files).length > 0 &&
                     Object.values(wsFiles.trash_files).filter(f => {
                         if (folder) {
@@ -34,6 +35,16 @@ const RemoveFiles = (props) => {
                             return f.folder_id === null;
                         }
                     }).map(f => {
+                        return (
+                            <FileListItem key={f.id} isMember={isMember}
+                                        forceDelete={true}
+                                        className="col-xl-3 col-lg-4 col-md-6 col-sm-12" 
+                                        file={f} actions={actions}/>
+                        );
+                    })
+                    :
+                    wsFiles && Object.values(wsFiles.trash_files).length > 0 &&
+                    Object.values(wsFiles.trash_files).map(f => {
                         return (
                             <FileListItem key={f.id} isMember={isMember}
                                         forceDelete={true}
