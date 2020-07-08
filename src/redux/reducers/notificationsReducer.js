8c413a164368ba85361c8c84cad1662fdf3c8c0d
Nilo Cruz <nilo@makedevelopment.com>
2020-07-08 19:24:28 +0800

    Driff-323
    Added new post, post comment to notification

--- src/redux/reducers/notificationsReducer.js
+++ src/redux/reducers/notificationsReducer.js
@@ -65,6 +65,60 @@ export default (state = INITIAL_STATE, action) => {
                 notifications: {}
             }
         }
+        case "INCOMING_POST": {
+            if (action.data.author.id !== state.user.id) {
+                let postNotification = {
+                    id: action.data.notification.id,
+                    type: "POST_CREATE",
+                    is_read: 0,
+                    created_at: action.data.created_at,
+                    author: action.data.author,
+                    data: {
+                        post_id: action.data.id,
+                        type: action.data.type,
+                        must_read: action.data.is_must_read,
+                        must_reply: action.data.is_must_reply,
+                        personalized_for_id: action.data.personalized_for_id,
+                        title: action.data.title,
+                        workspaces: action.data.workspaces,
+                        comment_body: null
+                    }
+                }
+                return {
+                    ...state,
+                    notifications: {...state.notifications, [postNotification.id]: postNotification}
+                }
+            } else {
+                return state
+            }
+        }
+        case "INCOMING_COMMENT": {
+            if (action.data.author.id !== state.user.id) {
+                let postNotification = {
+                    id: action.data.notification.id,
+                    type: action.data.notification.type,
+                    is_read: 0,
+                    created_at: action.data.created_at,
+                    author: action.data.author,
+                    data: {
+                        post_id: action.data.post_id,
+                        type: "TOPIC_POST",
+                        must_read: 0,
+                        must_reply: 0,
+                        personalized_for_id: action.data.personalized_for_id,
+                        title: action.data.post_title,
+                        workspaces: action.data.workspaces,
+                        comment_body: action.data.body
+                    }
+                }
+                return {
+                    ...state,
+                    notifications: {...state.notifications, [postNotification.id]: postNotification}
+                }
+            } else {
+                return state
+            }
+        }
         default:
             return state;
     }
