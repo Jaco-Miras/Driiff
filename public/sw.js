c0b1fd4aa2f7193a95aaa21bdd508c1de6304b81
Nilo Cruz <nilo@makedevelopment.com>
2020-08-14 00:10:48 +0800

    DRIFF-491
    Updated service worker options for post and post comment

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
 function receivePushNotification(event) {
     console.log("[Service Worker] Push Received.", event, event.data.json());
   
-    const { reference_title, id, slug, channel_code, strip_body, user } = event.data.json();
-    const options = {
+    const { reference_title, id, slug, channel_code, strip_body, user, author, title, body, SOCKET_TYPE } = event.data.json();
+    let options = {
       data: `https://${slug}.drevv.com/chat/${channel_code}`,
       body: strip_body,
       icon: user.profile_image_link,
@@ -12,22 +12,38 @@ function receivePushNotification(event) {
       badge: "./favicon.ico",
       actions: [{ action: "Detail", title: "View", icon: "./favicon.ico" }]
     };
-
+    let notification_title = "";
     //Get URL objects for each client's location.
-    let isOnSelectedChatChannel = false
-    let isWindowVisible = false
+    let showNotification = false;
     self.clients.matchAll({includeUncontrolled: true}).then(clients => {
       for (const client of clients) {
         const clientUrl = new URL(client.url);
-        if (clientUrl.pathname === `/chat/${channel_code}`) {
-          isOnSelectedChatChannel = true
+        if (SOCKET_TYPE === "CHAT_CREATE" && clientUrl.pathname === `/chat/${channel_code}`) {
+          showNotification = true;
+          notification_title = reference_title;
+        } else if (SOCKET_TYPE === "POST_COMMENT_CREATE") {
+          notification_title = `${author.name} replied in`;
+          options = {
+            ...options,
+            body: body.replace(/(<([^>]+)>)/gi, ""),
+            image: author.profile_image_link,
+            icon: author.profile_image_link,
+          }
+        } else if (SOCKET_TYPE === "POST_CREATE") {
+          notification_title = `${author.name} shared a post`;
+          options = {
+            ...options,
+            body: title,
+            image: author.profile_image_link,
+            icon: author.profile_image_link,
+          }
         }
         if (client.visibilityState === 'visible') {
-          isWindowVisible = true
+          showNotification = true
         }
       }
-      if (!isOnSelectedChatChannel || !isWindowVisible){
-        event.waitUntil(self.registration.showNotification(reference_title, options));
+      if (showNotification){
+        self.registration.showNotification(notification_title, options)
       }
     });
   }
