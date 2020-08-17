5f289512cd456ec6b1ea0edb599549381c37a7d8
Nilo Cruz <nilo@makedevelopment.com>
2020-08-17 20:40:45 +0800

    DRIFF-491
    Updated service worker notification
    applied redirect link on service worker notification

--- public/sw.js
+++ public/sw.js
@@ -1,51 +1,91 @@
 function receivePushNotification(event) {
     console.log("[Service Worker] Push Received.", event, event.data.json());
   
-    const { reference_title, id, slug, channel_code, strip_body, user, author, title, body, SOCKET_TYPE } = event.data.json();
+    const { reference_title, id, channel_code, strip_body, user, author, title, body, redirect_link, code_data, workspaces, SOCKET_TYPE } = event.data.json();
     let options = {
-      data: `https://${slug}.drevv.com/chat/${channel_code}`,
+      data: redirect_link,
       body: strip_body,
-      icon: user.profile_image_link,
       vibrate: [200, 100, 200],
       tag: id,
-      image: user.profile_image_link,
       badge: "./favicon.ico",
       actions: [{ action: "Detail", title: "View", icon: "./favicon.ico" }]
     };
-    let notification_title = "";
+    let notification_title = reference_title;
     //Get URL objects for each client's location.
     let showNotification = false;
-    self.clients.matchAll({includeUncontrolled: true}).then(clients => {
-      for (const client of clients) {
-        const clientUrl = new URL(client.url);
-        if (SOCKET_TYPE === "CHAT_CREATE" && clientUrl.pathname === `/chat/${channel_code}`) {
-          showNotification = true;
-          notification_title = reference_title;
-        } else if (SOCKET_TYPE === "POST_COMMENT_CREATE") {
-          notification_title = `${author.name} replied in a post`;
-          options = {
-            ...options,
-            body: body.replace(/(<([^>]+)>)/gi, ""),
-            image: author.profile_image_link,
-            icon: author.profile_image_link,
+    if (SOCKET_TYPE === "CHAT_DELETE") {
+      self.registration.getNotifications({tag: id}).then(notifications => {
+        notifications.forEach(notification => {
+          //console.log(notification)
+          //notification.body = "removed message"
+          notification.close()
+        })
+      })
+    } else {
+      const replaceChar = (name, char = "-") => {
+        return name.toLowerCase().replace(/\s|\//g, char);
+      };
+      self.clients.matchAll({includeUncontrolled: true}).then(clients => {
+        for (const client of clients) {
+          const clientUrl = new URL(client.url);
+          if (SOCKET_TYPE === "CHAT_CREATE" && clientUrl.pathname === `/chat/${channel_code}`) {
+            showNotification = true;
+            notification_title = reference_title;
+            options = {
+              ...options,
+              image: user.profile_image_link,
+              icon: user.profile_image_link,
+            }
+          } else if (SOCKET_TYPE === "POST_COMMENT_CREATE") {
+            const { base_link, push_title, post_id, post_title } = code_data;
+            notification_title = push_title;
+            let link = "";
+            if (workspaces.length) {
+              if (workspaces[0].workspace_id){
+                link = `/workspace/posts/${workspaces[0].workspace_id}/${replaceChar(workspaces[0].workspace_name)}/${workspaces[0].topic_id}/${replaceChar(workspaces[0].topic_name)}/post/${post_id}/${replaceChar(post_title)}`;
+              } else {
+                link = `/workspace/posts/${workspaces[0].topic_id}/${replaceChar(workspaces[0].topic_name)}/post/${post_id}/${replaceChar(post_title)}`;
+              }
+            } else {
+              link = `/posts/${post_id}/${replaceChar(post_title)}`;
+            }
+            options = {
+              ...options,
+              data: `${base_link}${link}`,
+              body: body.replace(/(<([^>]+)>)/gi, ""),
+              image: author.profile_image_link,
+              icon: author.profile_image_link,
+            }
+          } else if (SOCKET_TYPE === "POST_CREATE") {
+            const { base_link } = code_data;
+            notification_title = `${author.name} shared a post`;
+            let link = "";
+            if (workspaces.length) {
+              if (workspaces[0].workspace_id){
+                link = `/workspace/posts/${workspaces[0].workspace_id}/${replaceChar(workspaces[0].workspace_name)}/${workspaces[0].topic_id}/${replaceChar(workspaces[0].topic_name)}/post/${post_id}/${replaceChar(post_title)}`;
+              } else {
+                link = `/workspace/posts/${workspaces[0].topic_id}/${replaceChar(workspaces[0].topic_name)}/post/${post_id}/${replaceChar(post_title)}`;
+              }
+            } else {
+              link = `/posts/${id}/${replaceChar(title)}`;
+            }
+            options = {
+              ...options,
+              body: title,
+              data: `${base_link}${link}`,
+              image: author.profile_image_link,
+              icon: author.profile_image_link,
+            }
           }
-        } else if (SOCKET_TYPE === "POST_CREATE") {
-          notification_title = `${author.name} shared a post`;
-          options = {
-            ...options,
-            body: title,
-            image: author.profile_image_link,
-            icon: author.profile_image_link,
+          if (client.visibilityState !== 'visible') {
+            showNotification = true
           }
         }
-        if (client.visibilityState === 'visible') {
-          showNotification = true
+        if (showNotification){
+          self.registration.showNotification(notification_title, options)
         }
-      }
-      if (showNotification){
-        self.registration.showNotification(notification_title, options)
-      }
-    });
+      });
+    }
   }
   
   function openPushNotification(event) {
