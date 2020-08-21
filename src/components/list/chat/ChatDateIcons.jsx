6fd900f9a93cac2d6f3068727f1453af89c6bf82
Nilo Cruz <nilo@makedevelopment.com>
2020-08-21 16:10:52 +0800

    DRIFF-431
    fixed unread counters in channels

--- src/components/list/chat/ChatDateIcons.jsx
+++ src/components/list/chat/ChatDateIcons.jsx
@@ -36,7 +36,7 @@ const ChatDateIcons = (props) => {
   return (
     <Wrapper className="chat-timestamp">
       {
-        (!channel.is_read && channel.total_unread > 0) &&
+        (!channel.is_read || channel.total_unread > 0) &&
         <Badge className={`badge badge-primary badge-pill ml-auto ${!channel.is_read && channel.total_unread === 0 ? "unread" : ""}`}>
           {channel.total_unread > 0 ? channel.total_unread : !channel.is_read ? "0" : null}
         </Badge>
