f4624ef59a01e4db22332dc3bd2f7ae9b8cdf6ba
joeresu <joules.castillon@gmail.com>
2020-08-31 14:45:53 +0800

    DRIFF-648 show profile image

--- src/components/list/links/PersonalLinks.jsx
+++ src/components/list/links/PersonalLinks.jsx
@@ -123,7 +123,6 @@ const PersonalLinks = (props) => {
   useEffect(() => {
     if (ref.nav.current !== null) {
       let maxHeight = window.innerHeight * 5;
-      console.log(ref.nav.current)
       maxHeight = maxHeight < ref.nav.current.offsetHeight ? ref.nav.current.offsetHeight : maxHeight;
       setMaxHeight(maxHeight);
     }
