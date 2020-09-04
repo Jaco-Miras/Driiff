d9389cd4b6111514327762d3d0a4f40bb1bef029
joeresu <joules.castillon@gmail.com>
2020-09-04 17:05:29 +0800

    hotfix uploading files on folder

--- src/redux/services/files.js
+++ src/redux/services/files.js
@@ -419,7 +419,10 @@ export function postCompanyUploadFiles(payload) {
 }
 
 export function postCompanyUploadBulkFiles(payload) {
-  let url = `/v2/company/upload-bulk-files?${objToUrlParams(payload)}`;
+  let url = `/v2/company/upload-bulk-files`;
+  if (payload.folder_id) {
+    url += `?folder_id=${payload.folder_id}`;
+  }
   return apiCall({
     method: "POST",
     url: url,
