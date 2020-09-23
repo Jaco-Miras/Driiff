88b3bd77f3bccb3b6210bc8de96b4dd74e391e8a
Amin Pasalic <62027486+AminPasalic@users.noreply.github.com>
2020-09-23 15:58:55 +0200

    DRIFF-811

--- src/components/panels/post/company/CompanyPostBody.jsx
+++ src/components/panels/post/company/CompanyPostBody.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import {AvatarGroup, SvgIconFeather} from "../../../common";
-import {useGoogleApis, useTimeFormat} from "../../../hooks";
-import {CompanyPostBadge} from "./index";
+import { AvatarGroup, SvgIconFeather } from "../../../common";
+import { useGoogleApis, useTimeFormat } from "../../../hooks";
+import { CompanyPostBadge } from "./index";
 import quillHelper from "../../../../helpers/quillHelper";
+import Tooltip from "react-tooltip-lite";
 
 const Wrapper = styled.div`
   flex: unset;
@@ -13,16 +14,29 @@ const Wrapper = styled.div`
   }
 `;
 
+const toggleTooltip = () => {
+  let tooltips = document.querySelectorAll("span.react-tooltip-lite");
+  tooltips.forEach((tooltip) => {
+    tooltip.parentElement.classList.toggle("tooltip-active");
+  });
+};
+
+const StyledTooltip = styled(Tooltip)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Icon = styled(SvgIconFeather)`
   width: 16px;
   cursor: pointer;
 `;
 
 const CompanyPostBody = (props) => {
-  const {post, postActions, dictionary, disableOptions} = props;
+  const { post, postActions, dictionary, disableOptions } = props;
 
   const [star, setStar] = useState(post.is_favourite);
-  const {fromNow} = useTimeFormat();
+  const { localizeDate, fromNow } = useTimeFormat();
   const googleApis = useGoogleApis();
 
   const handleStarPost = () => {
@@ -49,8 +63,7 @@ const CompanyPostBody = (props) => {
   return (
     <Wrapper className="card-body">
       <div className="d-flex align-items-center p-l-r-0 m-b-20">
-        <div className="d-flex align-items-center">{post.users_responsible.length > 0 &&
-        <AvatarGroup users={post.users_responsible}/>}</div>
+        <div className="d-flex align-items-center">{post.users_responsible.length > 0 && <AvatarGroup users={post.users_responsible} />}</div>
         <div className="ml-auto d-flex align-items-center text-muted">
           {
             // !isAuthor && post.is_read_requirement &&
@@ -64,16 +77,19 @@ const CompanyPostBody = (props) => {
             //     </div>
             // </div>
           }
-          <CompanyPostBadge post={post} isBadgePill={true} dictionary={dictionary}/>
-          {post.files.length > 0 && <Icon className="mr-2" icon="paperclip"/>}
-          <Icon className="mr-2" onClick={handleStarPost} icon="star" fill={star ? "#ffc107" : "none"}
-                stroke={star ? "#ffc107" : "currentcolor"}/>
-          {!disableOptions && <Icon className="mr-2" onClick={handleArchivePost} icon="archive"/>}
-          <span className="text-muted">{fromNow(post.created_at.timestamp)}</span>
+          <CompanyPostBadge post={post} isBadgePill={true} dictionary={dictionary} />
+          {post.files.length > 0 && <Icon className="mr-2" icon="paperclip" />}
+          <Icon className="mr-2" onClick={handleStarPost} icon="star" fill={star ? "#ffc107" : "none"} stroke={star ? "#ffc107" : "currentcolor"} />
+          {!disableOptions && <Icon className="mr-2" onClick={handleArchivePost} icon="archive" />}
+          <div className={"time-stamp"}>
+            <StyledTooltip arrowSize={5} distance={10} onToggle={toggleTooltip} content={`${localizeDate(post.created_at.timestamp)}`}>
+              <span className="text-muted">{fromNow(post.created_at.timestamp)}</span>
+            </StyledTooltip>
+          </div>
         </div>
       </div>
       <div className="d-flex align-items-center">
-        <div ref={handlePostBodyRef} dangerouslySetInnerHTML={{__html: quillHelper.parseEmoji(post.body)}}/>
+        <div ref={handlePostBodyRef} dangerouslySetInnerHTML={{ __html: quillHelper.parseEmoji(post.body) }} />
       </div>
     </Wrapper>
   );
