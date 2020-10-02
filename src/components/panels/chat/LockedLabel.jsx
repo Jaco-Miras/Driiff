5fd9762c47a8e13f47a7cbac11680f61b2ae664e
Robbert <rtuerlings@zuid.com>
2020-10-02 16:45:32 +0200

    DRIFF-508

--- src/components/panels/chat/LockedLabel.jsx
+++ src/components/panels/chat/LockedLabel.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import {useTranslation} from "../../hooks";
 
 const Wrapper = styled.div`
   font-weight: 500;
   color: #9b9b9b;
   padding: 10px 10px 10px 0;
 `;
+
+const { _t } = useTranslation();
+
+const dictionary = {
+    chatInfoPrivateWorkspace: _t("CHAT.INFO_PRIVATE_WORKSPACE", "You are in a private workspace."),
+  };
+
 
 const LockedLabel = (props) => {
 
@@ -16,7 +24,7 @@ const LockedLabel = (props) => {
 
     if (channel && workspaces.hasOwnProperty(channel.entity_id)) {
         if (workspaces[channel.entity_id].is_lock === 1 && workspaces[channel.entity_id].active === 1 && !channel.is_archived) {
-            return <Wrapper>You are in a private workspace.</Wrapper>
+            return <Wrapper>{dictionary.chatInfoPrivateWorkspace}</Wrapper>
         } else {
             return null;
         }
