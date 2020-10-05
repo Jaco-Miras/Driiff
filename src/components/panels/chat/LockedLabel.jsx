7a24e9722c10a8af2d62486e90d6eea89325cc61
joeresu <joules.castillon@gmail.com>
2020-10-06 02:24:16 +0800

    DRIFF-508 implement translation for locked label

--- src/components/panels/chat/LockedLabel.jsx
+++ src/components/panels/chat/LockedLabel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
-import {useTranslation} from "../../hooks";
+import { useTranslation } from "../../hooks";
 
 const Wrapper = styled.div`
   font-weight: 500;
@@ -9,28 +9,25 @@ const Wrapper = styled.div`
   padding: 10px 10px 10px 0;
 `;
 
-const { _t } = useTranslation();
-
-const dictionary = {
-    chatInfoPrivateWorkspace: _t("CHAT.INFO_PRIVATE_WORKSPACE", "You are in a private workspace."),
-  };
-
-
 const LockedLabel = (props) => {
 
-    const { channel } = props;
+  const { className = "", channel } = props;
 
-    const workspaces = useSelector((state) => state.workspaces.workspaces);
+  const { _t } = useTranslation();
 
-    if (channel && workspaces.hasOwnProperty(channel.entity_id)) {
-        if (workspaces[channel.entity_id].is_lock === 1 && workspaces[channel.entity_id].active === 1 && !channel.is_archived) {
-            return <Wrapper>{dictionary.chatInfoPrivateWorkspace}</Wrapper>
-        } else {
-            return null;
-        }
-    } else {
-        return null;
+  const workspaces = useSelector((state) => state.workspaces.workspaces);
+
+  const dictionary = {
+    lockedLabel: _t("CHAT.INFO_PRIVATE_WORKSPACE", "You are in a private workspace.")
+  };
+
+  return (<>
+    {
+      channel && !channel.is_archived &&
+      workspaces.hasOwnProperty(channel.entity_id) && workspaces[channel.entity_id].is_lock === 1 && workspaces[channel.entity_id].active === 1 &&
+      <Wrapper className={`locked-label ${className}`}>{dictionary.lockedLabel}</Wrapper>
     }
+  </>);
 };
 
 export default LockedLabel;
