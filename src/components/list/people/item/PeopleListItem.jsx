7cd64c7fe8d05190c6e8164d80ecb98ea918d19c
Robbert Tuerlings <rtuerlings@zuid.com>
2020-10-15 12:37:55 +0200

    Update PeopleListItem.jsx
    Move item to left.

--- src/components/list/people/item/PeopleListItem.jsx
+++ src/components/list/people/item/PeopleListItem.jsx
@@ -126,9 +126,17 @@ const PeopleListItem = (props) => {
               </div>
               {onChatClick !== null && loggedUser.type !== "external" && (
                 <div className="button-wrapper">
+                  {user.contact !== "" && loggedUser.id !== user.id && (
+                    <a href={`tel:${user.contact.replace(/ /g, "").replace(/-/g, "")}`}>
+                      <SvgIconFeather className="mr-2" icon="phone" />
+                    </a>
+                  )}
                   {
+                    (user.type !== "external" && loggedUser.id !== user.id) && <SvgIconFeather onClick={handleOnChatClick} icon="message-circle" />
+                  }
+                                    {
                     showOptions && user.type !== "external" && user.role.name !== "owner" &&
-                    <MoreOptions className="mr-2" width={240} moreButton={"more-horizontal"}
+                    <MoreOptions className="ml-2" width={240} moreButton={"more-horizontal"}
                                  scrollRef={refs.cardBody.current}>
                       {user.role.name === "employee" &&
                       <div onClick={() => handleUpdateRole("admin")}>{dictionary.assignAsAdmin}</div>}
@@ -152,14 +160,6 @@ const PeopleListItem = (props) => {
                         )
                       } */}
                     </MoreOptions>
-                  }
-                  {user.contact !== "" && loggedUser.id !== user.id && (
-                    <a href={`tel:${user.contact.replace(/ /g, "").replace(/-/g, "")}`}>
-                      <SvgIconFeather className="mr-2" icon="phone" />
-                    </a>
-                  )}
-                  {
-                    (user.type !== "external" && loggedUser.id !== user.id) && <SvgIconFeather onClick={handleOnChatClick} icon="message-circle" />
                   }
                 </div>
               )}
