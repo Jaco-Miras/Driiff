b417af071a9825bd27bf27b3b6a9bba68f61d304
Robbert Tuerlings <rtuerlings@zuid.com>
2020-11-10 16:49:22 +0100

    Update CompanyPostItemPanel.jsx

--- src/components/panels/post/company/CompanyPostItemPanel.jsx
+++ src/components/panels/post/company/CompanyPostItemPanel.jsx
@@ -5,8 +5,6 @@ import { Avatar, SvgIconFeather } from "../../../common";
 import { MoreOptions } from "../../common";
 import { CompanyPostBadge } from "./index";
 import quillHelper from "../../../../helpers/quillHelper";
-import { MemberLists } from "../../../list/members";
-import { useTimeFormat, useTouchActions, useTranslation } from "../../../hooks";
 
 const Wrapper = styled.li`
   &.has-unread {
@@ -40,7 +38,7 @@ const Wrapper = styled.li`
     &.has-unread {
       font-weight: bold;
       
-      .post-partialBody {        
+      .post-partialBody {
         color: #343a40;
         font-weight: normal;
       
@@ -57,6 +55,7 @@ const Wrapper = styled.li`
     .time-stamp {
       margin-left: 1rem;
       font-weight: 400;
+      font-size: 11px;
     }
   }
 
@@ -78,7 +77,7 @@ const Wrapper = styled.li`
     }
   }
   .post-partialBody {
-    color: #b8b8b8;
+    color: #b8b8b8 !important;
     font-weight: 400;
   }
 
@@ -87,14 +86,21 @@ const Wrapper = styled.li`
   }
   .author-avatar {
     position: absolute;
-    left: 1rem;
-    top: 0;
-    bottom: 0;
-    margin: auto;
+    left: -1rem;
+    top: 1.3rem;
+    // bottom: 0;
+    // margin: auto;
     img {
       width: 2rem;
       height: 2rem;
     }
+  }
+  .receiver {
+    border-radius: 6px;
+    padding: 3px 5px;
+    background-color: rgba(210, 210, 210, 0.2);
+    font-size: 11px;
+    margin-right: 3px;
   }
 
   .ellipsis-hover {
@@ -159,6 +165,11 @@ const Icon = styled(SvgIconFeather)`
   width: 16px;
 `;
 
+const ByIcon = styled(SvgIconFeather)`
+  width: 16px;
+  stroke: lightgrey;
+`;
+
 const ArchiveBtn = styled.a`
   padding: 5px;
 `;
@@ -166,6 +177,23 @@ const ArchiveBtn = styled.a`
 const AuthorRecipients = styled.div`
   display: flex;
   align-items: center;
+  font-weight: 400;
+  padding-bottom: 3px;
+`;
+
+const CreatedBy = styled.div`
+  position: absolute;
+  top: 1rem;
+  left: 2rem;
+  bottom: 0;
+`;
+
+const PostReplyCounter = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 5px;
+  font-weight: 400;
+  font-size: 11px;
 `;
 
 const CompanyPostItemPanel = (props) => {
@@ -182,36 +210,33 @@ const CompanyPostItemPanel = (props) => {
   const { fromNow } = useTimeFormat();
 
   const postRecipients = useSelector((state) => state.global.recipients
-    .filter((r) => post.recipient_ids.includes(r.id))
-    .sort((a, b) => {
-      if (a.type !== b.type) {
-        if (a.type === "TOPIC") return -1;
-        if (b.type === "TOPIC") return 1;
-      }
-      return a.name.localeCompare(b.name);
+  .filter((r) => post.recipient_ids.includes(r.id))
+  .sort((a, b) => {
+    if (a.type !== b.type) {
+      if (a.type === "TOPIC") return -1;
+      if (b.type === "TOPIC") return 1;
+    }
+    return a.name.localeCompare(b.name);
     })
   );
 
   const renderUserResponsibleNames = () => {
-    let recipient_names = "@ ";
+    let recipient_names = "";
     const otherPostRecipients = postRecipients.filter(r => !(r.type === "USER" && r.type_id === user.id));
     const hasMe = postRecipients.some(r => r.type_id === user.id);
     if (otherPostRecipients.length) {
       recipient_names += otherPostRecipients.filter((r, i) => i < (hasMe ? 4 : 5))
-        .map(r => {
-          if (["DEPARTMENT", "TOPIC"].includes(r.type))
-            return `<span class="receiver">${_t(r.name.replace(/ /g, "_").toUpperCase(), r.name)}</span>`;
-          else
-            return `<span class="receiver">${r.name}</span>`;
-        })
         .join(`, `);
+        .join(``);
     }
 
     if (hasMe) {
       if (otherPostRecipients.length >= 1) {
-        recipient_names += `, ${dictionary.me}`;
+        // recipient_names += `, ${dictionary.me}`;
+        recipient_names += `<span class="receiver">${dictionary.me}</span>`;
       } else {
-        recipient_names += dictionary.me;
+        // recipient_names += dictionary.me;
+        recipient_names += `<span class="receiver">${dictionary.me}</span>`;
       }
     }
 
@@ -229,13 +254,6 @@ const CompanyPostItemPanel = (props) => {
     }
 
     return `${recipient_names} ${otherRecipientNames}`;
-  };
-
-  const handleMarkDone = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (disableOptions) return;
-    markPost(post);
   };
 
   const handleStarPost = (e) => {
@@ -277,7 +295,6 @@ const CompanyPostItemPanel = (props) => {
     handleSwipeRight
   });
 
-  const noAuthorResponsibles = post.users_responsible.filter(u => u.id !== post.author.id);
   const hasUnread = post.unread_count > 0 || post.is_unread === 1;
 
   return (
@@ -285,40 +302,37 @@ const CompanyPostItemPanel = (props) => {
              className={`list-group-item post-item-panel ${hasUnread ? "has-unread" : ""} ${className}`}
              onTouchStart={touchStart} onTouchMove={touchMove} onTouchEnd={touchEnd}
              onClick={() => openPost(post, "/posts")}>
-      {/* <div className="custom-control custom-checkbox custom-checkbox-success">
-        <CheckBox name="test" checked={post.is_mark_done} onClick={handleMarkDone} disabled={disableOptions}/>
-      </div> */}
-      {/* <div>
-        <Icon className="mr-2" icon="star" onClick={handleStarPost}
-              stroke={post.is_favourite ? "#ffc107" : "currentcolor"} fill={post.is_favourite ? "#ffc107" : "none"}/>
-      </div>  */}
       <div className="flex-grow-1 min-width-0">
         <div className="d-flex align-items-center justify-content-between">
           <div
-            className={`app-list-title ${hasUnread ? "has-unread" : ""}`}>
-            <Avatar title={`FROM: ${post.author.name}`} className="author-avatar mr-2" id={post.author.id}
-                    name={post.author.name}
-                    imageLink={post.author.profile_image_thumbnail_link ? post.author.profile_image_thumbnail_link : post.author.profile_image_link}/>
+            className={`app-list-title text-truncate ${hasUnread ? "has-unread" : ""}`}>
+            <CreatedBy>
+              <ByIcon icon="corner-up-right" />
+                <Avatar title={`FROM: ${post.author.name}`} className="author-avatar mr-2" id={post.author.id}
+                        name={post.author.name}
+                        imageLink={post.author.profile_image_thumbnail_link ? post.author.profile_image_thumbnail_link : post.author.profile_image_link}/>
+            </CreatedBy>
             <AuthorRecipients>
               {
                 postRecipients.length >= 1 &&
                 <span className="recipients" dangerouslySetInnerHTML={{ __html: renderUserResponsibleNames() }}/>
               }
-              <span className="time-stamp">
-                <span>{fromNow(post.created_at.timestamp)}</span>
-              </span>
             </AuthorRecipients>
             <span>{post.title}</span>
             <div className='text-truncate post-partialBody'>
               <span dangerouslySetInnerHTML={{ __html: quillHelper.parseEmoji(post.partial_body) }}/>
             </div>
+            <PostReplyCounter>
+              {post.unread_count !== 0 &&
+              <div className="mr-2 badge badge-secondary text-white text-9">{post.unread_count} new</div>}
+              <div className="text-muted">{post.reply_count} comments</div>
+              <span className="time-stamp text-muted">
+                <span>{fromNow(post.created_at.timestamp)}</span>
+              </span>
+            </PostReplyCounter>
           </div>
           <SlideOption showOptions={showOptions} className={`pl-sm-3 d-flex align-items-center`}>
-            {post.unread_count !== 0 &&
-            <div className="ml-2 mr-2 badge badge-primary badge-pill">{post.unread_count}</div>}
             <CompanyPostBadge post={post} dictionary={dictionary}/>
-            {noAuthorResponsibles && noAuthorResponsibles.length > 0 &&
-            <MemberLists members={noAuthorResponsibles} classNames="mr-2"/>}
             {!disableOptions &&
             <ArchiveBtn onClick={handleArchivePost} className="btn button-darkmode btn-outline-light ml-2"
                         data-toggle="tooltip"
@@ -329,7 +343,7 @@ const CompanyPostItemPanel = (props) => {
         </div>
       </div>
       {post.type !== "draft_post" && !disableOptions && (
-        <MoreOptions className="ml-2" item={post} width={220} moreButton={"more-horizontal"}>
+        <MoreOptions className={`d-flex ml-2`} item={post} width={220} moreButton={"more-horizontal"}>
           {
             post.todo_reminder === null &&
             <div onClick={() => remind(post)}>{dictionary.remindMeAboutThis}</div>
@@ -350,5 +364,3 @@ const CompanyPostItemPanel = (props) => {
     </Wrapper>
   );
 };
-
-export default React.memo(CompanyPostItemPanel);
