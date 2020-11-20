08ce8cfd14fb52cfa376d50f0ccb02fed40425d1
joasihe <joasihe90@gmail.com>
2020-11-20 09:32:09 +0100

    DRIFF-christmas

--- src/components/panels/company/CompanyChatPanel.jsx
+++ src/components/panels/company/CompanyChatPanel.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { ChatContentPanel, ChatSidebarPanel } from "../chat";
+import Snowfall from "react-snowfall";
+//christmas
 
 const Wrapper = styled.div`
   .chat-sidebar-panel {
@@ -49,12 +51,22 @@ const Wrapper = styled.div`
 
 const CompanyChatPanel = (props) => {
   const { className = "" } = props;
+  const [active, setActive] = useState(true);
+
+  //christmas
+  useEffect(() => {
+    setTimeout(() => {
+      setActive(false);
+    }, 7000);
+  });
 
   return (
     <Wrapper className={`company-chat ${className}`}>
       <div className="row no-gutters chat-block">
-        <ChatSidebarPanel className={"col-lg-4 chat-sidebar-panel"}/>
-        <ChatContentPanel className={"col-lg-8 chat-content-panel"}/>
+        <ChatSidebarPanel className={"col-lg-4 chat-sidebar-panel"} />
+        <ChatContentPanel className={"col-lg-8 chat-content-panel"} />
+        {active ? <Snowfall /> : ""}
+        {/* christmas */}
       </div>
     </Wrapper>
   );
