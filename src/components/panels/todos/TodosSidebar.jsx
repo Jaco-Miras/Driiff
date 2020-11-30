0fd6cc0119ddc38378576cec0e0fdbdc47ead773
joeresu <joules.castillon@gmail.com>
2020-11-30 15:57:31 +0800

    DRIFF-1056 - remove hover effect on todo items

--- src/components/panels/todos/TodosSidebar.jsx
+++ src/components/panels/todos/TodosSidebar.jsx
@@ -2,27 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { SvgIconFeather } from "../../common";
 
-const Wrapper = styled.div`
-`;
+const Wrapper = styled.div``;
 
 const Filter = styled.span`
   cursor: pointer;
-  
+
   .dark & {
     background: #191c20 !important;
     border-color: #ffffff14 !important;
   }
-    
-  &:hover,
   &.active {
-    background-color: #7a1b8b;
-    border-color: #7a1b8b;
     color: #fff;
-    
-    .dark & {
-      background: #ffffff14 !important;
-      border-color: #ffffff14 !important;
-    }  
   }
   &.folder-list {
     border-bottom: 1px solid rgba(0, 0, 0, 0.125);
@@ -44,14 +34,7 @@ const Icon = styled(SvgIconFeather)`
 `;
 
 const TodosSidebar = (props) => {
-  const {
-    className = "",
-    dictionary,
-    todoActions,
-    setFilter,
-    filter,
-    count
-  } = props;
+  const { className = "", dictionary, todoActions, setFilter, filter, count } = props;
 
   return (
     <Wrapper className={`todo-sidebar bottom-modal-mobile ${className}`}>
@@ -65,28 +48,20 @@ const TodosSidebar = (props) => {
         <div>
           <div className="app-sidebar-menu" tabIndex="1">
             <div className="list-group list-group-flush">
-              <Filter onClick={setFilter}
-                      data-filter=""
-                      className={`list-group-item d-flex align-items-center ${filter === "" ? "active" : ""}`}>
-                <Icon className="mr-2" icon="list"/>
+              <Filter onClick={setFilter} data-filter="" className={`list-group-item d-flex align-items-center ${filter === "" ? "active" : ""}`}>
+                <Icon className="mr-2" icon="list" />
                 {dictionary.statusToday}
               </Filter>
-              <Filter onClick={setFilter}
-                      data-filter="OVERDUE"
-                      className={`list-group-item d-flex align-items-center ${filter === "OVERDUE" ? "active" : ""}`}>
-                <Icon className="mr-2" icon="alert-circle"/>
+              <Filter onClick={setFilter} data-filter="OVERDUE" className={`list-group-item d-flex align-items-center ${filter === "OVERDUE" ? "active" : ""}`}>
+                <Icon className="mr-2" icon="alert-circle" />
                 {dictionary.statusOverdue} {/*{count.overdue}*/}
               </Filter>
-              <Filter onClick={setFilter}
-                      data-filter="NEW"
-                      className={`list-group-item d-flex align-items-center ${filter === "NEW" ? "active" : ""}`}>
-                <Icon className="mr-2" icon="clock"/>
+              <Filter onClick={setFilter} data-filter="NEW" className={`list-group-item d-flex align-items-center ${filter === "NEW" ? "active" : ""}`}>
+                <Icon className="mr-2" icon="clock" />
                 {dictionary.statusUpcoming} {/*{count.new}*/}
               </Filter>
-              <Filter onClick={setFilter}
-                      data-filter="DONE"
-                      className={`list-group-item d-flex align-items-center ${filter === "DONE" ? "active" : ""}`}>
-                <Icon className="mr-2" icon="check"/>
+              <Filter onClick={setFilter} data-filter="DONE" className={`list-group-item d-flex align-items-center ${filter === "DONE" ? "active" : ""}`}>
+                <Icon className="mr-2" icon="check" />
                 {dictionary.statusDone} {/*{count.done}*/}
               </Filter>
             </div>
