e6f8682d47885bf57bd81ba940e1cb35ef6b14d8
Nilo Cruz <nilo@makedevelopment.com>
2021-02-09 22:32:34 +0800

    DRIFF-1452
    Added published channel select component
    Added repeat type select component
    Added radio buttons
    Updated form state
    Updated radio buttons and added number times input
    Create repeat on component for monthly and yearly
    Updated repeat on weekly component
    Updated create huddle bot function
    Update prefill of huddle bot data when selecting channel that have huddle

--- src/components/common/DatePicker.jsx
+++ src/components/common/DatePicker.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from "react";
 import DatePicker from "react-date-picker";
 
 const DatePickerComponent = (props) => {
-
   const { className = "", calendarIcon = null, clearIcon = null, dayPlaceholder = "dd", monthPlaceholder = "mm", yearPlaceholder = "yy" } = props;
 
   const pickerRef = useRef(null);
@@ -14,16 +13,24 @@ const DatePickerComponent = (props) => {
     const _this = pickerRef.current;
 
     _this.onOutsideAction = (event) => {
-      console.log(event, "test");
       if (_this.wrapper && !_this.wrapper.contains(event.target)) {
         _this.closeCalendar();
       }
     };
   });
 
-  return <DatePicker className={`react-datetime-picker ${className}`} ref={pickerRef} calendarIcon={calendarIcon}
-                     clearIcon={clearIcon} dayPlaceholder={dayPlaceholder} monthPlaceholder={monthPlaceholder}
-                     yearPlaceholder={yearPlaceholder} {...props} />;
+  return (
+    <DatePicker
+      className={`react-datetime-picker ${className}`}
+      ref={pickerRef}
+      calendarIcon={calendarIcon}
+      clearIcon={clearIcon}
+      dayPlaceholder={dayPlaceholder}
+      monthPlaceholder={monthPlaceholder}
+      yearPlaceholder={yearPlaceholder}
+      {...props}
+    />
+  );
 };
 
 export default DatePickerComponent;
