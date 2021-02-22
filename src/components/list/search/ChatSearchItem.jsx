4731310b63fd70ec319c74ffe8c40c6a56ce729d
Nilo Cruz <nilo@makedevelopment.com>
2021-02-22 09:16:54 +0800

    DRIFF-1143
    Add channel title in chat search result
    Fetch channel when clicking chat search results

--- src/components/list/search/ChatSearchItem.jsx
+++ src/components/list/search/ChatSearchItem.jsx
@@ -3,31 +3,31 @@ import styled from "styled-components";
 import { Avatar } from "../../common";
 
 const Wrapper = styled.li`
-    display: flex;
-    width: 100%;
-    p {
-        margin: 0;
-    }
+  display: flex;
+  width: 100%;
+  p {
+    margin: 0;
+  }
 `;
 
 const ChatSearchItem = (props) => {
-
-  const {data, redirect} = props;
-  const {channel, message} = data;
+  const { data, redirect } = props;
+  const { channel, message } = data;
 
   const handleRedirect = () => {
-    redirect.toChat(channel, message)
+    redirect.toChat(channel, message);
   };
 
   return (
     <Wrapper className="list-group-item p-l-0 p-r-0">
       <div>
-        <Avatar id={message.user.id} name={message.user.name}
-                imageLink={message.user.profile_image_thumbnail_link ? message.user.profile_image_thumbnail_link : message.user.profile_image_link}/>
+        <Avatar id={message.user.id} name={message.user.name} imageLink={message.user.profile_image_thumbnail_link ? message.user.profile_image_thumbnail_link : message.user.profile_image_link} />
       </div>
       <div className="ml-2" onClick={handleRedirect}>
-        <p>{message.user.name}</p>
-        <p className="text-muted" dangerouslySetInnerHTML={{ __html: message.body }}/>
+        <p>
+          {message.user.name} - {channel.title}
+        </p>
+        <p className="text-muted" dangerouslySetInnerHTML={{ __html: message.body }} />
       </div>
     </Wrapper>
   );
