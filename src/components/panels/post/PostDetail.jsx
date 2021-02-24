56a62cba06fe42e6a3b2081e751dc95a8e81b6d7
Nilo Cruz <nilo@makedevelopment.com>
2021-02-24 18:53:29 +0800

    DRIFF-1498
    Update post options in post detail

--- src/components/panels/post/PostDetail.jsx
+++ src/components/panels/post/PostDetail.jsx
@@ -479,9 +479,7 @@ const PostDetail = (props) => {
                   {post.is_unread === 0 ? <div onClick={() => markAsUnread(post, true)}>{dictionary.markAsUnread}</div> : <div onClick={() => markAsRead(post, true)}>{dictionary.markAsRead}</div>}
                   <div onClick={() => sharePost(post)}>{dictionary.share}</div>
                   {post.author.id !== user.id && <div onClick={() => followPost(post)}>{post.is_followed ? dictionary.unFollow : dictionary.follow}</div>}
-                  {((post.author && post.author.id === user.id) || (post.author.type === "external" && user.type === "internal")) && (
-                    <div onClick={() => close(post)}>{post.is_close ? dictionary.openThisPost : dictionary.closeThisPost}</div>
-                  )}
+                  <div onClick={() => close(post)}>{post.is_close ? dictionary.openThisPost : dictionary.closeThisPost}</div>
                 </StyledMoreOptions>
               </li>
             </ul>
@@ -494,6 +492,7 @@ const PostDetail = (props) => {
                 <div onClick={() => markAsUnread(post, true)}>{dictionary.markAsUnread}</div>
                 <div onClick={() => sharePost(post)}>{dictionary.share}</div>
                 {post.author.id !== user.id && <div onClick={() => followPost(post)}>{post.is_followed ? dictionary.unFollow : dictionary.follow}</div>}
+                {post.author.type === "external" && user.type === "internal" && <div onClick={() => close(post)}>{post.is_close ? dictionary.openThisPost : dictionary.closeThisPost}</div>}
               </StyledMoreOptions>
             </div>
           )}
