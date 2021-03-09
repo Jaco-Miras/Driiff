3751d9a44d7c72f27dfc66b31ca60c821f7f07bc
Nilo Cruz <nilo@makedevelopment.com>
2021-03-09 14:02:59 +0800

    hotfix gift icon display condition

--- src/components/panels/main/MainNavigationTabPanel.jsx
+++ src/components/panels/main/MainNavigationTabPanel.jsx
@@ -442,18 +442,13 @@ const MainNavigationTabPanel = (props) => {
 
   return (
     <Wrapper className={`navigation-menu-tab ${className}`}>
-      <div>
-        <div className="navigation-menu-tab-header" data-toggle="tooltip" title="Driff" data-placement="right" data-original-title="Driff">
-          {/* {driffSettings.READ_RELEASE_UPDATES && userSettings.READ_RELEASE_UPDATES && driffSettings.READ_RELEASE_UPDATES.timestamp > userSettings.READ_RELEASE_UPDATES.timestamp && (
-            <GiftWrapper>
-              <GiftIcon icon="gift" color="#fff" onClick={handleGiftClick} />
-            </GiftWrapper>
-          )} */}
-
-          <div className="driff-logo">
-            <DriffLogo icon="driff-logo" data-link="/" onClick={handleIconClick} />
-          </div>
-        </div>
+      <div className="navigation-menu-tab-header" data-toggle="tooltip" title="Driff" data-placement="right" data-original-title="Driff">
+        {((driffSettings.READ_RELEASE_UPDATES && userSettings.READ_RELEASE_UPDATES && driffSettings.READ_RELEASE_UPDATES.timestamp > userSettings.READ_RELEASE_UPDATES.timestamp) || userSettings?.READ_RELEASE_UPDATES === null) && (
+          <GiftWrapper>
+            <GiftIcon icon="gift" color="#fff" onClick={handleGiftClick} />
+          </GiftWrapper>
+        )}
+        <DriffLogo icon="driff-logo2" data-link="/" onClick={handleIconClick} />
       </div>
       <div className="flex navigation-menu-tab-header-options">
         <ul>
