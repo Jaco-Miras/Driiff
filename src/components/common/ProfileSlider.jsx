089da944fc44fdbfdda8d4a1484b78b886b253c4
Nilo Cruz <nilo@makedevelopment.com>
2021-03-24 17:46:35 +0800

    DRIFF-1489
    Updated profile slider css
    Updated external view
    Added external label
    Applied translation

--- src/components/common/ProfileSlider.jsx
+++ src/components/common/ProfileSlider.jsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 //import { useSelector } from "react-redux";
 import Avatar from "./Avatar";
 import { SvgIconFeather } from "./SvgIcon";
-import { useOutsideClick, useUserChannels } from "../hooks";
+import Badge from "./Badge";
+import { useOutsideClick, useUserChannels, useTranslation } from "../hooks";
 
 const ProfileWrapper = styled.div``;
 const Icon = styled(SvgIconFeather)`
@@ -17,6 +18,23 @@ const ProfileSlider = (props) => {
   const [loading, setLoading] = useState(false);
 
   const sliderRef = useRef(null);
+
+  const { _t } = useTranslation();
+
+  const dictionary = {
+    companyName: _t("PROFILE.COMPANY_NAME", "Company name"),
+    information: _t("PROFILE.INFORMATION", "Information"),
+    firstName: _t("PROFILE.FIRST_NAME", "First name:"),
+    middleName: _t("PROFILE.MIDDLE_NAME", "Middle name:"),
+    lastName: _t("PROFILE.LAST_NAME", "Last name:"),
+    password: _t("PROFILE.PASSWORD", "Password:"),
+    position: _t("PROFILE.POSITION", "Position:"),
+    city: _t("PROFILE.CITY", "City:"),
+    address: _t("PROFILE.ADDRESS", "Address:"),
+    phone: _t("PROFILE.Phone", "Phone:"),
+    email: _t("PROFILE.EMAIL", "Email:"),
+    external: _t("PROFILE.EXTERNAL", "External"),
+  };
 
   let user = null;
   if (profile) {
@@ -54,23 +72,24 @@ const ProfileSlider = (props) => {
             <Icon icon="message-circle" loading={loading} />
           </button>
         )}
+        {user && user.type === "external" && loggedUser.type === "internal" && <Badge label={dictionary.external} badgeClassName="badge badge-info text-white mt-2" />}
       </div>
       <div className="information-wrapper">
         <div className="info-x">
-          <span>Information</span>
+          <span>{dictionary.information}</span>
         </div>
         <div className="d-flex">
           <div className="labels-wrapper">
-            <label>First Name</label>
-            <label>Last Name</label>
-            <label>Position</label>
-            <label>Email</label>
+            <label>{dictionary.firstName}</label>
+            <label>{dictionary.lastName}</label>
+            <label>{dictionary.position}</label>
+            {loggedUser.type === "internal" && <label>{dictionary.email}</label>}
           </div>
           <div className="info-details">
             <span>{user?.first_name}</span>
             <span>{user?.last_name}</span>
             <span>{user?.role && user?.role.display_name}</span>
-            <span>{user?.email}</span>
+            {loggedUser.type === "internal" && <span>{user?.email}</span>}
           </div>
         </div>
       </div>
