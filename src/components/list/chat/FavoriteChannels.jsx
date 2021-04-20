8733b636dfdd9b16ef38dca4b3c198846c969aa4
Nilo Cruz <nilo@makedevelopment.com>
2021-04-20 22:24:56 +0800

    Update on scroll bar

--- src/components/list/chat/FavoriteChannels.jsx
+++ src/components/list/chat/FavoriteChannels.jsx
@@ -14,6 +14,11 @@ const Wrapper = styled.div`
     overflow-x: scroll;
     display: flex;
     border-bottom: 1px solid #dee2e6;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
     .dark & {
       border-bottom-color: rgba(155, 155, 155, 0.2) !important;
     }
