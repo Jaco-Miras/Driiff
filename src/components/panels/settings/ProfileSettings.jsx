bc398ed6b7c6afdf660a1960a30eb198d7fde403
Nilo Cruz <nilo@makedevelopment.com>
2021-04-21 17:49:16 +0800

    Apply translation to live translation label

--- src/components/panels/settings/ProfileSettings.jsx
+++ src/components/panels/settings/ProfileSettings.jsx
@@ -113,6 +113,7 @@ const ProfileSettings = (props) => {
     workspaceSettingsTitle: _t("SETTINGS.WORKSPACE_TITLE", "Workspace Settings"),
     sortWorkspaceLabel: _t("SETTINGS.SORT_WORKSPACE_LABEL", "Sort workspace by"),
     viewRelease: _t("SETTINGS.VIEW_RELEASE", "View Release List"),
+    liveTranslation: _t("SETTINGS.LIVE_TRANSLATION", "Talk in your own language (live translation)"),
   };
 
   // const notificationSoundOptions = [
@@ -404,7 +405,7 @@ const ProfileSettings = (props) => {
                   </div>
                 </div>
               }
-{
+              {
                 <div className="row mb-3">
                   <div className="col-12">
                     <CustomInput
@@ -415,7 +416,7 @@ const ProfileSettings = (props) => {
                       name="translate"
                       onChange={handleChatSwitchToggle}
                       data-success-message={`You have turn ${translate ? "OFF" : "ON"} translate chat messages!`}
-                      label={<span>Talk in your own language (live translation)</span>}
+                      label={<span>{dictionary.liveTranslation}</span>}
                     />
                   </div>
                 </div>
