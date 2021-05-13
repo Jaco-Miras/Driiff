05a2e2d96d027bdfea7eeabec53fd672ba8d1dc5
Nilo Cruz <nilo@makedevelopment.com>
2021-05-13 18:34:34 +0800

    Updated reminder count

--- src/components/panels/main/MainSidebarLinks.jsx
+++ src/components/panels/main/MainSidebarLinks.jsx
@@ -123,6 +123,8 @@ const MainSidebarLinks = (props) => {
       return total;
     }, 0);
 
+  const reminderCount = count.today + count.overdue;
+
   return (
     <Wrapper className="flex navigation-menu-tab-header-options">
       <ul>
@@ -150,7 +152,7 @@ const MainSidebarLinks = (props) => {
             <NavIcon icon={"calendar"} />
             <div>{dictionary.todoLinks}</div>
             {/* <div>{count.overdue !== 0 && <Badge>&nbsp;</Badge>}</div> */}
-            {count.today > 0 && <Badge className={"badge badge-primary badge-pill ml-1"}>{count.today > 99 ? "99+" : count.today}</Badge>}
+            {reminderCount && <Badge className={"badge badge-primary badge-pill ml-1"}>{reminderCount > 99 ? "99+" : reminderCount}</Badge>}
           </NavIconContainer>
         </li>
         <li onClick={closeLeftNav}>
