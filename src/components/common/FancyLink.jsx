2323672604346e172fa47f7763957aa40f03d5a9
jadellad1 <jay.prior.dalleda@gmail.com>
2021-06-16 16:11:33 +0800

    Update FancyLink.jsx

--- src/components/common/FancyLink.jsx
+++ src/components/common/FancyLink.jsx
@@ -3,19 +3,20 @@ import styled from "styled-components";
 
 const Wrapper = styled.a`
 border-radius: 5px !important;
-background-color: #F8F8F8 !important;
+background-color: #FFF !important;
 box-shadow: 0 1px 1px 0 rgba(0,0,0,0.11) !important; 
 text-decoration: none !important;
 display: inline-block;
 height: auto !important;
 padding:5px 10px;
 margin-bottom: 5px;
-color: #696969 !important;
+color: #000 !important;
 font-family: Inter !important;
 font-size: 13px !important;
 letter-spacing: 0 !important;
 line-height: 21px !important;
 margin-right: 1pt;
+margin-left: 1pt;
 ::before { content : url(${props => props.favlink});  display: inline-block;vertical-align: middle;line-height: normal; padding-right: .3em;}
 `;
 const FancyLink = (props) => {
