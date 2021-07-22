31868dbc1b32f04456f49630bc71adf73c70e1bc
Nilo Cruz <nilo@makedevelopment.com>
2021-07-22 17:34:58 +0800

    DRIFF-1721
    Applied code splitting on main route

--- src/layout/routes.jsx
+++ src/layout/routes.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { useSettings } from "../components/hooks";
-import { TestChat } from "../components/test";
-import TestFiles from "../components/test/TestFiles";
-import GuestLayout from "./GuestLayout";
-import MainLayout from "./MainLayout";
+// import { TestChat } from "../components/test";
+// import TestFiles from "../components/test/TestFiles";
+//import GuestLayout from "./GuestLayout";
+//import MainLayout from "./MainLayout";
 import { addUserToReducers } from "../redux/actions/globalActions";
+
+const MainLayout = lazy(() => import("./MainLayout"));
+const GuestLayout = lazy(() => import("./GuestLayout"));
 
 export const AppRoute = ({ children, ...props }) => {
   const { init: settingsInit, fetch, fetchUserSettings, userSettings, driffSettings } = useSettings();
@@ -48,46 +51,50 @@ export const AppRoute = ({ children, ...props }) => {
 
   return session.authenticated ? (
     <>
+      <Suspense fallback={<div></div>}>
+        <Switch>
+          <Route {...props} component={GuestLayout} path={["/logged-out", "/force-logout"]} exact={true} />
+          {/* <Route {...props} component={TestFiles} path={["/test/files/workspace/:workspaceId"]}>
+            {children}
+          </Route>
+          <Route {...props} component={TestChat} path={["/test/chat"]}>
+            {children}
+          </Route> */}
+          <Route {...props} component={MainLayout} path={["/notifications", "/profile", "/dashboard", "/posts", "/chat", "/files", "/people", "/search", "/settings", "/system/people", "/todos", "/bot", "/releases"]}>
+            {children}
+          </Route>
+          <Route {...props} component={MainLayout} path={["/workspace/chat", "/workspace/:page", "/magic-link/:token"]}>
+            {children}
+          </Route>
+          <Redirect
+            path="*"
+            to={{
+              pathname: session.user.type === "external" ? "/workspace/chat" : "/chat",
+              state: { from: history.location },
+            }}
+          />
+        </Switch>
+      </Suspense>
+    </>
+  ) : (
+    <Suspense fallback={<div></div>}>
       <Switch>
-        <Route {...props} component={GuestLayout} path={["/logged-out", "/force-logout"]} exact={true} />
-        <Route {...props} component={TestFiles} path={["/test/files/workspace/:workspaceId"]}>
-          {children}
-        </Route>
-        <Route {...props} component={TestChat} path={["/test/chat"]}>
-          {children}
-        </Route>
-        <Route {...props} component={MainLayout} path={["/notifications", "/profile", "/dashboard", "/posts", "/chat", "/files", "/people", "/search", "/settings", "/system/people", "/todos", "/bot", "/releases"]}>
-          {children}
-        </Route>
-        <Route {...props} component={MainLayout} path={["/workspace/chat", "/workspace/:page", "/magic-link/:token"]}>
+        <Route
+          {...props}
+          component={GuestLayout}
+          path={["/driff-register", "/register", "/magic-link", "/resetpassword/:token/:email", "/reset-password", "/login", "/authenticate/:token/:returnUrl?", "/request-form", "/magic-link/:token"]}
+          exact
+        >
           {children}
         </Route>
         <Redirect
-          path="*"
+          from="*"
           to={{
-            pathname: session.user.type === "external" ? "/workspace/chat" : "/chat",
+            pathname: "/login",
             state: { from: history.location },
           }}
         />
       </Switch>
-    </>
-  ) : (
-    <Switch>
-      <Route
-        {...props}
-        component={GuestLayout}
-        path={["/driff-register", "/register", "/magic-link", "/resetpassword/:token/:email", "/reset-password", "/login", "/authenticate/:token/:returnUrl?", "/request-form", "/magic-link/:token"]}
-        exact
-      >
-        {children}
-      </Route>
-      <Redirect
-        from="*"
-        to={{
-          pathname: "/login",
-          state: { from: history.location },
-        }}
-      />
-    </Switch>
+    </Suspense>
   );
 };
