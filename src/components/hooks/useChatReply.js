f417a6e9b08e955156465085d0da6c88a89015f0
Nilo Cruz <nilo@makedevelopment.com>
2021-07-28 21:08:52 +0800

    DRIFF-1753
    disabled quillhelper and updated chat reducer

--- src/components/hooks/useChatReply.js
+++ src/components/hooks/useChatReply.js
@@ -360,7 +360,8 @@ const useChatReply = ({ reply, dictionary, isAuthor, user, selectedChannel, user
     }
   }
 
-  replyBody = parseSystemMessage(quillHelper.parseEmoji(stripGif(replyBody)));
+  // replyBody = parseSystemMessage(quillHelper.parseEmoji(stripGif(replyBody)));
+  replyBody = parseSystemMessage(stripGif(replyBody));
 
   let quoteAuthor = "";
   let quoteBody = "";
@@ -422,7 +423,7 @@ const useChatReply = ({ reply, dictionary, isAuthor, user, selectedChannel, user
       });
     }
 
-    quoteBody += quillHelper.parseEmoji(reply.quote.body);
+    //quoteBody += quillHelper.parseEmoji(reply.quote.body);
     quoteBody = parseSystemMessage(quoteBody);
   }
 
