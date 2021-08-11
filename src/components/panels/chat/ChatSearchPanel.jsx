3610b161a6da76bd83a299f11403270195d60a0b
jadellad1 <jay.prior.dalleda@gmail.com>
2021-08-12 03:19:10 +0800

    Update ChatSearchPanel.jsx

--- src/components/panels/chat/ChatSearchPanel.jsx
+++ src/components/panels/chat/ChatSearchPanel.jsx
@@ -6,13 +6,94 @@ import { useTimeFormat, useTranslationActions, useRedirect } from "../../hooks";
 import { getChatMsgsSearch } from "../../../redux/services/chat";
 
 const Wrapper = styled.div`
-position:absolute;top:0;bottom:0;right:0;width:100%;height:100%;transform:translateX(100%);transition:.1s ease-out;color:#eee;padding:0 1em;margin-right:-10px;background:#FFF;z-index:3;
-${(props) => props.isActive && `transform: translateX(0%);margin:0px;`}`
+position: absolute;
+top: 0;
+bottom: 0;
+right: 0;
+width: 100%;
+height: 100%;
+transform: translateX(100%);
+transition: 0.1s ease-out;
+padding: 0 1em;
+margin-right: -10px;
+z-index: 3;
+background:#FFF;
+color:#eee;
+${(props) => props.isActive && `transform: translateX(0%);margin:0px;`}
+.dark & {
+  background:#191c20;
+  color:#c7c7c7;
+}`;
+
 const BackIcon = styled(SvgIconFeather)`cursor: pointer; `;
-const ResultWrapper = styled.ul`list-style:none;background:transparent;margin:0;padding:0;li:last-child{border-bottom:none}`;
-const ResultItem = styled.li`cursor:pointer;background:transparent;font-size:0.835re;padding:5px 20px;margin-top:0;border-radius:0;border-left:none;border-right:none;border-top:none;border-bottom:1px solid #ebebeb;.dark &{border-color:rgba(155,155,155,.1)}color:#495057;p.chat-search-date{font-size:smaller;margin:0}div.chat-search-body{font-size:0.835re}div.chat-search-body span{font-weight:600;color:#7A1B8B}`;
+
+const ResultWrapper = styled.ul`list-style: none;
+background: transparent;
+margin: 0;
+padding: 0;
+li:last-child {
+    border-bottom: none;
+}
+`;
+const ResultItem = styled.li`cursor: pointer;
+background: transparent;
+font-size: 0.835re;
+padding: 5px 20px;
+margin-top: 0;
+border-radius: 0;
+border-left: none;
+border-right: none;
+border-top: none;
+border-bottom: 1px solid #ebebeb;
+.dark & {
+    border-color: rgba(155, 155, 155, 0.1);
+    color:#c7c7c7;
+}
+color: #495057;
+p.chat-search-date {
+    font-size: smaller;
+    margin: 0;
+}
+div.chat-search-body {
+    font-size: 0.835re;
+}
+div.chat-search-body span {
+    font-weight: 600; 
+    color: #7a1b8b;
+    .dark & {
+      color:#fff;
+  }
+}
+`;
 const ResultDivWrapper = styled.div`overflow: auto;height: calc(100% - 43px);`;
-const EmptyState = styled.div`display:flex;flex-flow:column;justify-content:center;align-items:center;flex-grow:1;padding:5rem;svg{display:block;margin:0 auto}h3{font-size:16px}h5{margin-bottom:0;font-size:14px}button{width:auto!important;margin:2rem auto}color:#363636;`;
+const EmptyState = styled.div`
+display: flex;
+flex-flow: column;
+justify-content: center;
+align-items: center;
+flex-grow: 1;
+padding: 5rem;
+color:#eee;
+.dark & {
+  color:#c7c7c7;
+}
+svg {
+    display: block;
+    margin: 0 auto;
+}
+h3 {
+    font-size: 16px;
+}
+h5 {
+    margin-bottom: 0;
+    font-size: 14px;
+}
+button {
+    width: auto !important;
+    margin: 2rem auto;
+}
+`;
+
 const SearchForm = styled.form`flex-grow:1;margin:0 .5rem 0 0.5em!important;.form-control{border-radius:8px!important;padding-left:40px;transition:padding-left 0.4s cubic-bezier(.275,.42,0,1);&:focus{padding-left:12px}}.not-empty .form-control{padding-left:12px}.not-empty .input-group-append{display:none}.btn-cross{right:0!important}.input-group-append{position:absolute;top:2px;left:0;button{border:0!important;padding:10px 14px!important;color:#aaa;pointer-events:none}}@media (max-width:480px){margin:0 0 0.75rem!important}&::placeholder{color:#aaa}.btn-cross{position:absolute;right:45px;opacity:0;border:0;background:transparent;padding:0;height:100%;width:36px;border-radius:4px;svg{width:16px;color:#495057}}.not-empty .btn-cross{z-index:9;opacity:1}.loading{height:1rem;width:1rem}`;
 
 const ChatSearchPanel = (props) => {
@@ -68,7 +149,7 @@ const ChatSearchPanel = (props) => {
     const resp = data.map((i) => {
       return i.map((item) => { return <ResultItem onClick={(e) => handleRedirect(item)} key={item.id}><p className="chat-search-date"> {todoFormat(item.created_at.timestamp)}</p> <div className="chat-search-body mb-2" dangerouslySetInnerHTML={{ __html: item.body }}></div> </ResultItem> });
     });
-    return (resp[0] && resp[0].length) ? resp : (<EmptyState> <h3>{dictionary.noItemsFoundHeader}</h3><h5>{dictionary.noItemsFoundText} </h5> </EmptyState>)
+    return (resp[0] && resp[0].length) ? resp : (<EmptyState > <h3>{dictionary.noItemsFoundHeader}</h3><h5>{dictionary.noItemsFoundText} </h5> </EmptyState>)
   };
 
   const getChatMsgs = (query, skip, fresh = false) => {
