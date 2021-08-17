a52ac17053d5d0e80170aa0f245be8b851e2941e
Nilo Cruz <nilo@makedevelopment.com>
2021-08-17 23:48:54 +0800

    DRIFF-1770
    Updated reducer for incoming post, insert to company timeline
    Updated reducer for deleted post, delete post in company timeline or workspace timeline

--- src/components/hooks/useCompanyDashboard.js
+++ src/components/hooks/useCompanyDashboard.js
@@ -1,40 +1,48 @@
-import {useEffect} from "react";
-import {useDashboardActions} from "./index";
-import {useSelector} from "react-redux";
+import { useEffect } from "react";
+import { useDashboardActions } from "./index";
+import { useSelector } from "react-redux";
 
 const useCompanyDashboard = () => {
-
   const actions = useDashboardActions();
-  const {init: timelineInit, skip: timelineSkip, limit: timelineLimit, has_more: timelineHasMore, items: timelineItems} = useSelector((state) => state.dashboard.timeline);
-  const {init: membersInit, skip: membersSkip, limit: membersLimit, has_more: membersHasMore, items: membersItems} = useSelector((state) => state.dashboard.members);
-  const {init: recentPostsInit, skip: recentPostsSkip, limit: recentPostsLimit, has_more: recentPostsHasMore, items: recentPostsItems} = useSelector((state) => state.dashboard.recent_posts);
+  const { init: timelineInit, skip: timelineSkip, limit: timelineLimit, has_more: timelineHasMore, items: timelineItems } = useSelector((state) => state.dashboard.timeline);
+  const { init: membersInit, skip: membersSkip, limit: membersLimit, has_more: membersHasMore, items: membersItems } = useSelector((state) => state.dashboard.members);
+  const { init: recentPostsInit, skip: recentPostsSkip, limit: recentPostsLimit, has_more: recentPostsHasMore, items: recentPostsItems } = useSelector((state) => state.dashboard.recent_posts);
 
   const fetchMoreTimeline = (callback) => {
     if (timelineHasMore) {
-      actions.fetchCompanyTimeline({
-        skip: timelineSkip,
-        limit: timelineLimit
-      }, callback)
+      actions.fetchCompanyTimeline(
+        {
+          skip: timelineSkip,
+          limit: timelineLimit,
+        },
+        callback
+      );
     }
-  }
+  };
 
   const fetchMoreRecentPosts = (callback) => {
     if (recentPostsHasMore) {
-      actions.fetchCompanyRecentPosts({
-        skip: recentPostsSkip,
-        limit: recentPostsLimit
-      }, callback)
+      actions.fetchCompanyRecentPosts(
+        {
+          skip: recentPostsSkip,
+          limit: recentPostsLimit,
+        },
+        callback
+      );
     }
-  }
+  };
 
   const fetchMoreMembers = (callback) => {
     if (membersHasMore) {
-      actions.fetchCompanyMembers({
-        skip: membersSkip,
-        limit: membersLimit
-      }, callback)
+      actions.fetchCompanyMembers(
+        {
+          skip: membersSkip,
+          limit: membersLimit,
+        },
+        callback
+      );
     }
-  }
+  };
 
   useEffect(() => {
     if (!timelineInit) {
@@ -58,7 +66,7 @@ const useCompanyDashboard = () => {
     recentPostsInit,
     recentPostsItems,
     membersInit,
-    membersItems
+    membersItems,
   };
 };
 
--- src/redux/reducers/dashboardReducer.js
+++ src/redux/reducers/dashboardReducer.js
@@ -4,22 +4,22 @@ const INITIAL_STATE = {
     skip: 0,
     limit: 1000,
     has_more: true,
-    items: {}
+    items: {},
   },
   members: {
     init: false,
     skip: 0,
     limit: 1000,
     has_more: true,
-    items: {}
+    items: {},
   },
   recent_posts: {
     init: false,
     skip: 0,
     limit: 1000,
     has_more: true,
-    items: {}
-  }
+    items: {},
+  },
 };
 
 export default (state = INITIAL_STATE, action) => {
@@ -29,40 +29,80 @@ export default (state = INITIAL_STATE, action) => {
       timeline.init = true;
       timeline.skip += timeline.limit;
 
-      action.data.timeline.forEach(t => {
+      action.data.timeline.forEach((t) => {
         timeline.items[t.id] = t;
-      })
+      });
 
       return {
-        ...state
-      }
+        ...state,
+      };
     }
     case "GET_COMPANY_DASHBOARD_RECENT_POST_SUCCESS": {
       let recent_posts = state.recent_posts;
       recent_posts.init = true;
       recent_posts.skip += recent_posts.limit;
 
-      action.data.recent_posts.forEach(rp => {
+      action.data.recent_posts.forEach((rp) => {
         recent_posts.items[rp.id] = rp;
-      })
+      });
 
       return {
         ...state,
-        recent_posts: recent_posts
-      }
+        recent_posts: recent_posts,
+      };
     }
     case "GET_COMPANY_DASHBOARD_MEMBERS_SUCCESS": {
       let members = state.members;
       members.init = true;
       members.skip += members.limit;
 
-      action.data.members.forEach(m => {
+      action.data.members.forEach((m) => {
         members.items[m.id] = m;
-      })
+      });
 
       return {
         ...state,
         members: members,
+      };
+    }
+    case "INCOMING_DELETED_POST": {
+      return {
+        ...state,
+        timeline: {
+          ...state.timeline,
+          items: Object.values(state.timeline.items)
+            .filter((timeline) => !(timeline.tag === "POST" && timeline.item && timeline.item.id === action.data.post_id))
+            .reduce((acc, t) => {
+              acc[t.id] = t;
+              return acc;
+            }, {}),
+        },
+      };
+    }
+    case "INCOMING_POST": {
+      if (action.data.SOCKET_TYPE === "POST_CREATE" && action.data.recipients.some((r) => r.type === "DEPARTMENT") && Object.keys(state.timeline.items).length > 0) {
+        return {
+          ...state,
+          timeline: {
+            ...state.timeline,
+            items: {
+              ...state.timeline.items,
+              [action.data.created_at.timestamp]: {
+                id: action.data.created_at.timestamp,
+                tag: "POST",
+                item: {
+                  body: action.data.body,
+                  created_at: { timestamp: action.data.created_at.timestamp },
+                  id: action.data.id,
+                  title: action.data.title,
+                  user: action.data.author,
+                },
+              },
+            },
+          },
+        };
+      } else {
+        return state;
       }
     }
     default:
