024911b56b881ba6b6b2f08e0bc98a4b76a46d72
Nilo Cruz <nilo@makedevelopment.com>
2021-09-09 00:34:02 +0800

    test update

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const http = require("http");
 
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const baseUrl = "demo24.drevv.com";
+
+app.get("/user-profile/:id", (req, res) => {
+  console.log(req.params);
+  var connector = http.request(
+    {
+      host: baseUrl,
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    },
+    (resp) => {
+      console.log(resp);
+      resp.pipe(res);
+    }
+  );
+
+  req.pipe(connector);
 });
 
 app.listen(9000, function () {
