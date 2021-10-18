0cee51ddfe2bff7c93cf758024880285bd110803
Nilo Cruz <nilo@makedevelopment.com>
2021-10-18 16:49:53 +0800

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.453310cbe9ea5f787e38dce80f2cdb06.js"
+  "/precache-manifest.b81f7f8927c1b9bdba6b68cbeefdc83d.js"
 );
 
 self.addEventListener('message', (event) => {
