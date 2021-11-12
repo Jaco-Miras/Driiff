8affd533257be7b5c4ec36f2c48f7559756345ec
Nilo Cruz <nilo@makedevelopment.com>
2021-11-12 17:02:31 +0800

    relocate newrelic to first line

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require("newrelic");
 const express = require("express");
 const path = require("path");
 const app = express();
-//require("newrelic");
 
 app.use(express.static(path.join(__dirname, "build")));
 
