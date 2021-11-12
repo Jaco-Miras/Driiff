348db329fae4c8c9cd34e5778d398be1db6fcfee
Nilo Cruz <nilo@makedevelopment.com>
2021-11-12 16:55:20 +0800

    remove relic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-require("newrelic");
+//require("newrelic");
 
 app.use(express.static(path.join(__dirname, "build")));
 
