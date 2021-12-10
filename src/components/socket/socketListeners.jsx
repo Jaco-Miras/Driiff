cf0081abfb53d13fedf2493f391aaac8944593df
Nilo Cruz <nilo@makedevelopment.com>
2021-12-10 17:21:44 +0800

    DRIFF-1936
    Fetch unread counters when websocket reconnects

--- src/components/socket/socketListeners.jsx
+++ src/components/socket/socketListeners.jsx
@@ -207,6 +207,7 @@ class SocketListeners extends Component {
   };
 
   refetch = () => {
+    this.props.getUnreadNotificationCounterEntries({ add_unread_comment: 1 });
     if (this.props.lastReceivedMessage && this.props.lastReceivedMessage.id) {
       this.props.refetchMessages({ message_id: this.props.lastReceivedMessage.id });
     }
