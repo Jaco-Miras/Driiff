94acbe8ec990afd3d0df7644a40fc081785d9375
Nilo Cruz <nilo@makedevelopment.com>
2021-12-15 19:53:07 +0800

    DRIFF-1947
    Update zoom icon color
    Show confirmation modal to start zoom meeting

--- src/components/common/SvgIcon.jsx
+++ src/components/common/SvgIcon.jsx
@@ -821,6 +821,16 @@ export const SvgIconFeather = React.memo(
           </>
         );
         break;
+      case "zoom":
+        viewBox = "0 0 48 48";
+        content = (
+          <>
+            <circle cx="24" cy="24" r="20" fill="#2196f3" />
+            <path fill="#fff" d="M29,31H14c-1.657,0-3-1.343-3-3V17h15c1.657,0,3,1.343,3,3V31z" />
+            <polygon fill="#fff" points="37,31 31,27 31,21 37,17" />
+          </>
+        );
+        break;
       case "meet":
         content = (
           <>
