1e207c50b3c5da40b9236b4f02030f85fdc2fd41
Nilo Cruz <nilo@makedevelopment.com>
2021-12-20 00:56:22 +0800

    DRIFF-1945
    Fixed text not disappearing when sending chat with file attachment

--- src/components/modals/FileUploadModal.jsx
+++ src/components/modals/FileUploadModal.jsx
@@ -476,6 +476,7 @@ const FileUploadModal = (props) => {
       setLoading(true);
       uploadFiles();
       dispatch(clearModal({ type: type }));
+      dispatch(saveInputData({ sent: true }));
     }
   };
 
