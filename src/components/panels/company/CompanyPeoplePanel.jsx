5062f6d4e2280b3a4ae154dc597407faf6a8c528
nilo-makedev <nilo@makedevelopment.com>
2022-01-27 00:12:23 +0800

    DRIFF-2003
    Remove people view toggle on company people
    Display invited count if invited toggle is on

--- src/components/panels/company/CompanyPeoplePanel.jsx
+++ src/components/panels/company/CompanyPeoplePanel.jsx
@@ -31,13 +31,28 @@ const Wrapper = styled.div`
 `;
 
 const Search = styled(SearchForm)`
-  width: 50%;
-  margin-bottom: 1rem;
+  //width: 50%;
+  //margin-bottom: 1rem;
   min-width: 250px;
   padding-right: 14px;
   @media all and (max-width: 991.99px) {
     width: 100%;
     padding-right: 0;
+  }
+`;
+
+const PeopleSearch = styled.div`
+  margin-bottom: 1rem;
+  .custom-switch {
+    margin-bottom: 0 !important;
+  }
+  div.text-muted {
+    font-weight: 500;
+  }
+  form {
+    display: flex;
+    flex-grow: 1;
+    width: auto;
   }
 `;
 
@@ -74,6 +89,15 @@ const CompanyPeoplePanel = (props) => {
     refs.search.current.focus();
   }, []);
 
+  // const botCodes = ["gripp_bot_account", "gripp_bot_invoice", "gripp_bot_offerte", "gripp_bot_project", "gripp_bot_account", "driff_webhook_bot", "huddle_bot"];
+  // const allUsers = Object.values(users).filter((u) => {
+  //   if (u.email && botCodes.includes(u.email)) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // });
+
   const userSort = Object.values(users)
     .sort((a, b) => {
       return a.name.toString().localeCompare(b.name);
@@ -101,10 +125,10 @@ const CompanyPeoplePanel = (props) => {
     peopleView: _t("PEOPLE.PEOPLE_VIEW_TOGGLE", "People view"),
   };
 
-  const handlePeopleViewToggle = () => {
-    setPeopleView(!peopleView);
-    setStructureView(!structureView);
-  };
+  // const handlePeopleViewToggle = () => {
+  //   setPeopleView(!peopleView);
+  //   setStructureView(!structureView);
+  // };
 
   const handleStructureViewToggle = () => {
     setStructureView(!structureView);
@@ -118,9 +142,9 @@ const CompanyPeoplePanel = (props) => {
       <div className="card">
         <div className="card-body">
           <div className="people-header">
-            <div className="d-flex align-items-center people-search">
+            <PeopleSearch className="d-flex align-items-center people-search">
               <Search ref={refs.search} value={search} closeButton="true" onClickEmpty={emptySearchInput} placeholder={dictionary.searchPeoplePlaceholder} onChange={handleSearchChange} autoFocus />
-              <CustomInput
+              {/* <CustomInput
                 className="ml-2 mb-3 cursor-pointer text-muted cursor-pointer"
                 checked={peopleView}
                 id="people_view"
@@ -129,9 +153,9 @@ const CompanyPeoplePanel = (props) => {
                 onChange={handlePeopleViewToggle}
                 data-success-message={`${peopleView ? "Inactive users are shown" : "Inactive users are no longer visible"}`}
                 label={<span>{dictionary.peopleView}</span>}
-              />
+              /> */}
               <CustomInput
-                className="ml-2 mb-3 cursor-pointer text-muted cursor-pointer"
+                className="ml-2 mr-3 cursor-pointer text-muted cursor-pointer"
                 checked={structureView}
                 id="structure_view"
                 name="structure_view"
@@ -140,7 +164,8 @@ const CompanyPeoplePanel = (props) => {
                 //data-success-message={`${showInactive ? "Inactive users are shown" : "Inactive users are no longer visible"}`}
                 label={<span>{dictionary.structureView}</span>}
               />
-            </div>
+              <div className="mr-3 text-muted">Total accounts: {userSort.length}</div>
+            </PeopleSearch>
           </div>
           {structureView && <CompanyStructure users={userSort} />}
           {peopleView && (
