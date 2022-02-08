f0a2bf0255ea6c0c4ba49b22a0479dcf3818bb88
nilo-makedev <nilo@makedevelopment.com>
2022-02-09 00:04:39 +0800

    DRIFF-2001
    Updated reducer of fetch post read and claps

--- src/redux/reducers/workspacesReducer.js
+++ src/redux/reducers/workspacesReducer.js
@@ -4101,6 +4101,7 @@ export default (state = INITIAL_STATE, action) => {
                           last_read_timestamp: r.last_read_timestamp,
                           profile_image_link: r.user.profile_image_link,
                           type: r.user.type,
+                          name: r.user.name,
                         };
                       }),
                     },
