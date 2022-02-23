f49929b276cb61a2f24fe277e5fba782048b7781
nilo-makedev <nilo@makedevelopment.com>
2022-02-24 00:00:48 +0800

    DRIFF-2085
    Show members list on hover

--- src/components/panels/admin/AllWorkspaceListDetail.jsx
+++ src/components/panels/admin/AllWorkspaceListDetail.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { SvgIconFeather } from "../../common";
+import Viewers from "../../list/post/item/Viewers";
 
 const Wrapper = styled.div`
   .workspace-title {
@@ -20,6 +21,75 @@ const Wrapper = styled.div`
     .mobile-icon {
       display: block;
     }
+  }
+  .user-reads-container {
+    position: relative;
+    display: inline-flex;
+    margin-right: 0.5rem;
+
+    .not-read-users-container,
+    .read-users-container {
+      transition: all 0.5s ease;
+      z-index: 2;
+      position: absolute;
+      right: 0;
+      top: 30px;
+      border: 1px solid #dee2e6;
+      border-radius: 6px;
+      background-color: #fff;
+      overflow: auto;
+      opacity: 0;
+      max-height: 0;
+      max-width: 200px;
+      text-align: left;
+
+      &:hover {
+        opacity: 1;
+        max-height: 175px;
+        max-width: 200px;
+        text-align: left;
+      }
+
+      .dark & {
+        background-color: #25282c;
+        border: 1px solid #25282c;
+      }
+
+      > span {
+        padding: 0.5rem;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        .avatar {
+          img {
+            min-width: 2.3rem;
+          }
+        }
+
+        .name {
+          width: 100%;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: block;
+        }
+      }
+    }
+  }
+
+  .user-reads-container {
+    span.not-readers:hover ~ span.not-read-users-container,
+    span.no-readers:hover ~ span.read-users-container {
+      opacity: 1;
+      max-height: 175px;
+      max-width: 200px;
+      text-align: left;
+    }
+  }
+  .avatar {
+    min-width: 2.7rem;
+    min-height: 2.7rem;
   }
 `;
 
@@ -42,9 +112,12 @@ const AllWorkspaceListDetail = (props) => {
             {item.workspace.name}
           </span>
         )}
-        <span className="ml-1 d-flex align-items-center">
-          <Icon icon="user" />
-          {Object.values(item.members).length}
+        <span className="ml-1 d-flex align-items-center user-reads-container">
+          <span className="no-readers">
+            <Icon icon="user" />
+            {Object.values(item.members).length}
+          </span>
+          <Viewers users={item.members} />
         </span>
 
         {item.is_shared && (
@@ -56,18 +129,6 @@ const AllWorkspaceListDetail = (props) => {
           </>
         )}
       </div>
-      {/* <div className="labels">
-        <span className="mr-2">
-          <Icon icon="user" />
-          {Object.values(item.members).length}
-        </span>
-        {item.workspace && (
-          <span className="mr-2">
-            <Icon icon="folder" />
-            {item.workspace.name}
-          </span>
-        )}
-      </div> */}
     </Wrapper>
   );
 };
