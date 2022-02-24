fac72cde8c656328fe6369d7c94d61d36bd4ca8c
nilo-makedev <nilo@makedevelopment.com>
2022-02-24 19:00:23 +0800

    DRIFF-2094
    Updated workspace posts loader

--- src/components/hooks/usePostActions.js
+++ src/components/hooks/usePostActions.js
@@ -67,6 +67,7 @@ import {
   refetchUnreadCompanyPosts,
   readNotification,
   getPostReadAndClap,
+  setShowUnread,
 } from "../../redux/actions/postActions";
 import { getUnreadWorkspacePostEntries, updateWorkspacePostCount, getFavoriteWorkspaceCounters, updateWorkspacePostFilterSort } from "../../redux/actions/workspaceActions";
 import { useToaster, useTodoActions } from "./index";
@@ -1026,6 +1027,10 @@ const usePostActions = () => {
     dispatch(getPostReadAndClap(payload, callback));
   };
 
+  const setShowUnreadPosts = (value) => {
+    dispatch(setShowUnread(value));
+  };
+
   return {
     approve,
     approveComment,
@@ -1081,6 +1086,7 @@ const usePostActions = () => {
     refetchCompanyPosts,
     readPostNotification,
     fetchPostReadAndClap,
+    setShowUnreadPosts,
   };
 };
 
