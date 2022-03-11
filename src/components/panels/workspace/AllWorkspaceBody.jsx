166cb0635dd1a714f711efdc0f93baaf104d183f
jeff <jeff.salarium@gmail.com>
2022-03-11 21:17:01 +0800

    remove previous implementation of related workspace

--- src/components/panels/workspace/AllWorkspaceBody.jsx
+++ src/components/panels/workspace/AllWorkspaceBody.jsx
@@ -1,11 +1,7 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { SvgIconFeather } from "../../common";
+import { SvgIconFeather, ToolTip } from "../../common";
 import WorkspaceListItem from "./WorkspaceListItem";
-import { ToolTip } from "../../common";
-import useQueryParams from "../../hooks/useQueryParams";
-import { useUsers } from "../../hooks";
-import { useEffect } from "react";
 const Wrapper = styled.div`
   overflow: visible !important;
 `;
@@ -57,27 +53,6 @@ const AllWorkspaceBody = (props) => {
   const [showWorkspaces, setShowWorkspaces] = useState({ showActive: true, showArchived: true });
   const [sortWorkspaces, setSortWorkspaces] = useState({ activeDate: true, archivedDate: true }); // default is date else alphabet
 
-  const [connectedUserLabel, setConnectedUserLabel] = useState(dictionary.noSharedWorkspace);
-
-  const { users } = useUsers();
-  const { params } = useQueryParams();
-  const userId = params ? params["user-id"] : null;
-
-  const connectedUser = useMemo(() => {
-    if (userId && filterBy === "all") {
-      return users[userId];
-    }
-    return null;
-  }, [userId, users, filterBy]);
-
-  useEffect(() => {
-    if (results.length > 0) {
-      setConnectedUserLabel(dictionary.workspaceYouShareWith);
-    } else {
-      setConnectedUserLabel(dictionary.noSharedWorkspace);
-    }
-  }, [results]);
-
   const handleShowWorkspaces = (type) => {
     setShowWorkspaces({
       ...showWorkspaces,
@@ -92,19 +67,8 @@ const AllWorkspaceBody = (props) => {
     });
   };
 
-  const renderConnectedUserLabel = () => {
-    if (connectedUser) {
-      return (
-        <p className="mx-3 my-2">
-          {connectedUserLabel}: {connectedUser.name}
-        </p>
-      );
-    }
-  };
-
   return (
     <Wrapper className={"card"}>
-      {renderConnectedUserLabel()}
       <Lists className="active-workspaces">
         <ListsHeader className="list-group-item">
           <span className="badge badge-light" onClick={() => handleShowWorkspaces("showActive")}>
