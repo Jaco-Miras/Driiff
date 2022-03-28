6b26ed5449ac362d45b4aa2fd531f308fb818b94
nilo-makedev <nilo@makedevelopment.com>
2022-03-28 23:47:29 +0800

    Revert "Merge branch 'DRIFF-1944' of https://github.com/MAKE-interactive/Driff-Frontend"
    This reverts commit 42362e74b1a6a385d8a7669541adf7b9e7f01c19, reversing
    changes made to 2e96d203a232cd17b6178b1bb633031a5680d052.

--- src/components/hooks/useEnlargeEmoticons.js
+++ src/components/hooks/useEnlargeEmoticons.js
@@ -1,5 +1,3 @@
-import { parseEmojis } from "../../helpers/stringFormatter";
-
 const useEnlargeEmoticons = (args = { enlargeEmoji: false }) => {
   const enlargeEmoji = (textWithHtml) => {
     let el = textWithHtml;
@@ -8,15 +6,13 @@ const useEnlargeEmoticons = (args = { enlargeEmoji: false }) => {
       el.innerHTML = textWithHtml;
     }
     const pattern = /((?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*)/g; // regex for emoji characters
-    const body = parseEmojis(el.textContent);
-    const bodyWithoutEmoji = body.trim().replace(pattern, ""); //removes all emoji instance
+    const bodyWithoutEmoji = el.textContent.trim().replace(pattern, ""); //removes all emoji instance
     const isEmojiWithString = typeof bodyWithoutEmoji === "string" && bodyWithoutEmoji.trim() !== ""; //check if body has text and emoji
-    const isMultipleEmojisOnly = body.trim().match(pattern) && body.trim().match(pattern).length > 1; //if message is only emoji but multiple
+    const isMultipleEmojisOnly = el.textContent.trim().match(pattern) && el.textContent.trim().match(pattern).length > 1; //if message is only emoji but multiple
     if (isEmojiWithString || isMultipleEmojisOnly) {
-      return body.replace(pattern, '<span class="font-size-24 line-height-32">$1</span>');
+      return el.innerHTML.replace(pattern, '<span class="font-size-24 line-height-32">$1</span>');
     }
-
-    return body.replace(pattern, '<div class="mx-3 my-3 font-size-40">$1</div>');
+    return args.enlargeSingle ? el.innerHTML.replace(pattern, '<div class="mx-3 my-3 font-size-40">$1</div>') : el.innerHTML;
   };
 
   return {
