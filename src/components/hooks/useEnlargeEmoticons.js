b33f374a5d221dc2e5eedc6d6589ea90166310af
jeff <jeff.salarium@gmail.com>
2022-03-29 21:55:24 +0800

    fix multiline issue

--- src/components/hooks/useEnlargeEmoticons.js
+++ src/components/hooks/useEnlargeEmoticons.js
@@ -2,14 +2,17 @@ import { parseEmojis } from "../../helpers/stringFormatter";
 
 const useEnlargeEmoticons = (args = { enlargeEmoji: false }) => {
   const enlargeEmoji = (textWithHtml) => {
+    let body = "";
     let el = textWithHtml;
     if (typeof textWithHtml === "string") {
       el = document.createElement("div");
       el.innerHTML = textWithHtml;
+      body = parseEmojis(textWithHtml);
+    } else {
+      body = parseEmojis(textWithHtml.innerHTML);
     }
     const pattern = /((?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*)/g; // regex for emoji characters
-    const body = parseEmojis(el.textContent);
-    const bodyWithoutEmoji = body.trim().replace(pattern, ""); //removes all emoji instance
+    const bodyWithoutEmoji = el.textContent.trim().replace(pattern, ""); //removes all emoji instance
     const isEmojiWithString = typeof bodyWithoutEmoji === "string" && bodyWithoutEmoji.trim() !== ""; //check if body has text and emoji
     const isMultipleEmojisOnly = body.trim().match(pattern) && body.trim().match(pattern).length > 1; //if message is only emoji but multiple
     if (isEmojiWithString || isMultipleEmojisOnly) {
