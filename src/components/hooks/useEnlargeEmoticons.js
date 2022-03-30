57404e0f989567f26c5a9829a022040b90d3e895
jeff <jeff.salarium@gmail.com>
2022-03-30 20:16:43 +0800

    enlarge emoticons

--- src/components/hooks/useEnlargeEmoticons.js
+++ src/components/hooks/useEnlargeEmoticons.js
@@ -2,6 +2,7 @@ import { parseEmojis } from "../../helpers/stringFormatter";
 
 const useEnlargeEmoticons = (args = { enlargeEmoji: false }) => {
   const enlargeEmoji = (textWithHtml) => {
+    debugger;
     let body = "";
     let el = textWithHtml;
     if (typeof textWithHtml === "string") {
@@ -12,14 +13,14 @@ const useEnlargeEmoticons = (args = { enlargeEmoji: false }) => {
       body = parseEmojis(textWithHtml.innerHTML);
     }
     const pattern = /((?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*)/g; // regex for emoji characters
-    const bodyWithoutEmoji = el.textContent.trim().replace(pattern, ""); //removes all emoji instance
+    const bodyWithoutEmoji = parseEmojis(el.textContent).trim().replace(pattern, ""); //removes all emoji instance
     const isEmojiWithString = typeof bodyWithoutEmoji === "string" && bodyWithoutEmoji.trim() !== ""; //check if body has text and emoji
     const isMultipleEmojisOnly = body.trim().match(pattern) && body.trim().match(pattern).length > 1; //if message is only emoji but multiple
     if (isEmojiWithString || isMultipleEmojisOnly) {
       return body.replace(pattern, '<span class="font-size-24 line-height-32">$1</span>');
     }
 
-    return body.replace(pattern, '<div class="mx-3 my-3 font-size-40">$1</div>');
+    return body.replace(pattern, '<div class="mx-3 my-4 font-size-40">$1</div>');
   };
 
   return {
