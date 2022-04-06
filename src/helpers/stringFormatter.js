6cb4b0d1e0f8a3b20c7eb074a68cdf58b8dc0fb3
jeff <jeff.salarium@gmail.com>
2022-04-06 21:59:21 +0800

    local implementation of replaceAll

--- src/helpers/stringFormatter.js
+++ src/helpers/stringFormatter.js
@@ -192,3 +192,11 @@ export const capitalizeFirstLetter = (value) => {
 export const hasCurrencySymbol = (value) => {
   return /[€£$]/g.test(value);
 };
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+}
+
+export const replaceAll = (str, find, replace) => {
+  return str.replace(new RegExp(escapeRegExp(find), "g"), replace);
+};
