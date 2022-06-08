dbd14780db0b9fa59d28fba5e548f7012d672d09
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-06-08 18:01:47 +0800

    update

--- src/components/panels/VideoMeeting.jsx
+++ src/components/panels/VideoMeeting.jsx
@@ -1,62 +1,9 @@
-import React, { useRef, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useRef, } from "react";
+import { useDispatch, } from "react-redux";
 import { JaaSMeeting } from "@jitsi/react-sdk";
-
-import styled from "styled-components";
 import { isMobile } from "react-device-detect";
 import { clearJitsi } from "../../redux/actions/chatActions";
-import { JitsiMeeting } from "@jitsi/react-sdk";
-
-import Draggable from "react-draggable";
 import { useParams } from "react-router-dom";
-
-const Wrapper = styled.div`
-  position: absolute;
-  z-index: 9999;
-  width: 70%;
-  height: 80%;
-  top: 50%;
-  left: 50%;
-  cursor: move;
-  padding: 1rem;
-  background: #29323f;
-  padding-top: 10px;
-  .j-container {
-    width: calc(100% - 30px);
-    height: calc(100% - 30px);
-    > div:first-child {
-      width: 100%;
-      height: 100%;
-    }
-  }
-  .buttons-container {
-    display: flex;
-    svg {
-      width: 1rem;
-      height: 1rem;
-      color: #fff;
-      cursor: pointer;
-    }
-  }
-  &.minimize {
-    width: 350px;
-    height: 300px;
-  }
-  .react-draggable-transparent-selection .draggable-iframe-cover {
-    display: block;
-    z-index: 999999;
-  }
-
-  .draggable-iframe-cover {
-    position: absolute;
-    display: none;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-  }
-`;
-
 
 const VideoMeeting = (props) => {
   const params = useParams();
@@ -64,25 +11,6 @@ const VideoMeeting = (props) => {
   const dispatch = useDispatch();
   const apiRef = useRef(null);
 
-  //const [activeDrags, setActiveDrags] = useState(0);
-  const [isDragging, setIsDragging] = useState(false);
-  const [controlledPosition, setControlledPosition] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  const onStart = () => {
-    setIsDragging(true);
-  };
-
-  const onStop = () => {
-    setIsDragging(false);
-  };
-
-  const onControlledDrag = (e, position) => {
-    const { x, y } = position;
-    setControlledPosition({ x, y });
-  };
 
   const handleClearJitsi = () => {
     dispatch(clearJitsi());
