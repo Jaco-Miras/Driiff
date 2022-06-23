ffe3a71a43db0f32e39936369c6ee7784c560b4a
Vincent <48564305+VncntDzn@users.noreply.github.com>
2022-06-24 00:06:09 +0800

    update

--- src/components/panels/VideoMeeting.jsx
+++ src/components/panels/VideoMeeting.jsx
@@ -14,7 +14,6 @@ const VideoMeeting = (props) => {
 
   const handleClearJitsi = () => {
     dispatch(clearJitsi());
-    window.webkit.messageHandlers.closeDriffTalkWindow.postMessage();
   };
 
   return (
