6a7ec6028d502045585db710af3daa1f2b119a3b
nilo-makedev <nilo@makedevelopment.com>
2022-07-26 22:34:33 +0800

    DRIFF-2516
    fixed reducer on new workspace

--- src/components/socket/socketListeners.jsx
+++ src/components/socket/socketListeners.jsx
@@ -2593,7 +2593,7 @@ class SocketListeners extends Component {
             sharedSlug: true,
           });
           this.props.getSharedChannels({ skip: 0, limit: 15, sharedPayload: { slug: e.topic.slug_owner, token: res.data[e.topic.slug_owner].access_token, is_shared: true } });
-          this.props.getNotifications({ skip: 0, limit: 50, sharedPayload: { slug: e.topic.slug_owner, token: res.data[e.topic.slug_owner].access_token, is_shared: true } });
+          //this.props.getNotifications({ skip: 0, limit: 50, sharedPayload: { slug: e.topic.slug_owner, token: res.data[e.topic.slug_owner].access_token, is_shared: true } });
         });
       });
   }
