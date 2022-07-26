d623003950ba73e77e5719580755af0c81e8f14f
nilo-makedev <nilo@makedevelopment.com>
2022-07-26 22:04:30 +0800

    DRIFF-2499
    Updated fetching of read and unread posts

--- src/components/hooks/useSharedCompanyPosts.js
+++ src/components/hooks/useSharedCompanyPosts.js
@@ -5,31 +5,46 @@ import { useSelector } from "react-redux";
 const useSharedCompanyPosts = () => {
   const sharedCompanyPosts = useSelector((state) => state.posts.sharedCompanyPosts);
   const sharedWs = useSelector((state) => state.workspaces.sharedWorkspaces);
+  const session = useSelector((state) => state.session);
   const actions = usePostActions();
   useEffect(() => {
-    if (Object.keys(sharedWs).length) {
-      //fetch the company post
-      Object.keys(sharedWs).forEach((slug) => {
-        if (sharedCompanyPosts[slug]) {
-          //check the existing shared company posts
-        } else {
-          //initial fetch
-          const sharedPayload = { slug: slug, token: sharedWs[slug].access_token, is_shared: true };
+    //fetch the company post
+    const sharedWorkspaces = session.user && session.user.sharedWorkspaces ? session.user.sharedWorkspaces : Object.keys(sharedWs).length ? sharedWs : [];
+    Object.keys(sharedWorkspaces).forEach((slug) => {
+      if (sharedCompanyPosts[slug]) {
+        //check the existing shared company posts
+        if (sharedCompanyPosts[slug].readPosts && sharedCompanyPosts[slug].readPosts.has_more) {
           actions.fetchSharedUnreadCompanyPosts({
-            skip: 0,
+            skip: sharedCompanyPosts[slug].readPosts.next_skip,
             limit: 25,
             filters: ["green_dot"],
-            sharedPayload,
-          });
-          actions.fetchSharedReadCompanyPosts({
-            skip: 0,
-            limit: 25,
-            filters: ["green_dot"],
-            sharedPayload,
+            sharedPayload: { slug: slug, token: sharedWorkspaces[slug].access_token, is_shared: true },
           });
         }
-      });
-    }
+        if (sharedCompanyPosts[slug].unreadPosts && sharedCompanyPosts[slug].unreadPosts.has_more) {
+          actions.fetchSharedReadCompanyPosts({
+            skip: sharedCompanyPosts[slug].unreadPosts.next_skip,
+            limit: 25,
+            filters: ["read_post"],
+            sharedPayload: { slug: slug, token: sharedWorkspaces[slug].access_token, is_shared: true },
+          });
+        }
+      } else {
+        //initial fetch
+        actions.fetchSharedUnreadCompanyPosts({
+          skip: 0,
+          limit: 25,
+          filters: ["green_dot"],
+          sharedPayload: { slug: slug, token: sharedWorkspaces[slug].access_token, is_shared: true },
+        });
+        actions.fetchSharedReadCompanyPosts({
+          skip: 0,
+          limit: 25,
+          filters: ["read_post"],
+          sharedPayload: { slug: slug, token: sharedWorkspaces[slug].access_token, is_shared: true },
+        });
+      }
+    });
   }, []);
 };
 
